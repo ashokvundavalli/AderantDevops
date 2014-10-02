529d8c0e07ff234b192ee0afebfe3d415d0c5ff4
Michael Baker <michael.baker@aderant.com>
2014-10-02 03:39:47 +0000

    Disable tools version 12.0 until VS2013 template is in production ***NO_CI***
    git-tfs-id: [http://tfs:8080/tfs/Aderant]$/ExpertSuite/Main/Modules/Build.Infrastructure;C246834

--- Src/Sources/Aderant.Build/Process/DefinitionParametersConfigurator.cs
+++ Src/Sources/Aderant.Build/Process/DefinitionParametersConfigurator.cs
@@ -15,12 +15,20 @@ namespace Aderant.Build.Process {
             if (configuration != null) {
                 parameters["ConfigurationFolderPath"] = configuration.ServerPathToModule + "/Build";
             }
-            
-            parameters["ToolsVersion"] = "12.0";
+
+            FixupPre2013Parameter(parameters);
 
             SetBuildSettings(parameters);
 
             buildDefinition.ProcessParameters = WorkflowHelpers.SerializeProcessParameters(parameters);
+        }
+
+        private static void FixupPre2013Parameter(IDictionary<string, object> parameters) {
+            // TODO: Enable after VS2013 upgrade
+            if (parameters.ContainsKey("ToolsVersion")) {
+                parameters.Remove("ToolsVersion");
+            }
+            //parameters["ToolsVersion"] = "12.0";
         }
 
         private static void SetBuildSettings(IDictionary<string, object> parameters) {
