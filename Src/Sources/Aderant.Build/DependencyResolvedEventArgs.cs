283a6bd11ac55c61e5d988fc5ef6b2c50941df2b
Michael Baker <michael.baker@aderant.com>
2015-04-17 06:12:17 +0000

    Add missing file ***NO_CI***
    git-tfs-id: [http://tfs:8080/tfs/Aderant]$/ExpertSuite/Main/Modules/Build.Infrastructure;C257171

--- Src/Sources/Aderant.Build/DependencyResolvedEventArgs.cs
+++ Src/Sources/Aderant.Build/DependencyResolvedEventArgs.cs
@@ -4,7 +4,11 @@ namespace Aderant.Build {
 
     internal sealed class DependencyResolvedEventArgs : EventArgs {
         public string DependencyProvider { get; set; }
+
         public string Branch { get; set; }
+
         public bool ResolvedUsingHardlink { get; set; }
+
+        public string FullPath { get; set; }
     }
 }
