4f77090f8397266cff47b4a60544a8bd9eb46220
Michael Baker <michael.baker@aderant.com>
2016-06-03 14:21:14 +1200

    Add function wrapper to bootstrap script

--- Build.ps1
+++ Build.ps1
@@ -1,26 +1,28 @@
-﻿try {
-    $cwd = (Split-Path -Parent $MyInvocation.MyCommand.Path)
+﻿function install
+{
+    try {
+        $cwd = (Split-Path -Parent $MyInvocation.MyCommand.Path)
 
-    $BUILDUTIL_TAG = "master"
+        $BUILDUTIL_TAG = "master"
 
-    $id = [Guid]::NewGuid().ToString()
-    $buildutil = [System.IO.Path]::Combine($cwd, "build.infrastructure." + $id)
+        $id = [Guid]::NewGuid().ToString()
+        $buildutil = [System.IO.Path]::Combine($cwd, "build.infrastructure." + $id)
 
-    gci -Path $cwd -Filter "build.infrastructure.*" | Remove-Item -Force -Recurse
+        gci -Path $cwd -Filter "build.infrastructure.*" | Remove-Item -Force -Recursee
 
-	# PowerShell will raise NativeCommandError if git writes to stdout or stderr
-	# therefore 2> is added and the output is eaten	
-    & "git" clone http://tfs:8080/tfs/ADERANT/ExpertSuite/_git/Build.Infrastructure $buildutil 2> git_error.log        
+        # PowerShell will raise NativeCommandError if git writes to stdout or stderr
+        # therefore 2> is added and the output is eaten	
+        & "git" clone http://tfs:8080/tfs/ADERANT/ExpertSuite/_git/Build.Infrastructure $buildutil 2> git_error.log        
   
-    cd $buildutil
+        cd $buildutil
 
-    & "git" fetch -a -v 2> git_error.log
-    & "git" fetch --tags 2> git_error.log
-    & "git" reset --hard $BUILDUTIL_TAG 2> git_error.log
-
-    [Environment]::SetEnvironmentVariable("BUILD_UTIL_DIRECTORY", $buildutil, "Process")
-        
-    .\Src\Build\BuildModule.ps1 -moduleToBuildPath $cwd -getDependencies $true -dropRoot "\\na.aderant.com\ExpertSuite\Main"
-} finally {
-    cd $cwd    
+        & "git" fetch -a -v 2> git_error.log
+        & "git" fetch --tags 2> git_error.log
+        & "git" reset --hard $BUILDUTIL_TAG 2> git_error.log
+    } finally {
+        cd $cwd    
+    }
+    write "Build bootstrap complete"
 }
+
+install @args
