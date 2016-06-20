04b0bf66d3d2b55a0f9edf727f9bc94b9310a3e6
Michael Baker <michael.baker@aderant.com>
2016-06-20 16:12:34 +1200

    Remove local compile of VSIX tools as this is now being published to the package server

---
+++ Package.ps1
@@ -0,0 +1,8 @@
+$paket = "$PSScriptRoot\Src\Build.Tools\paket.exe"
+
+& $paket restore
+
+& $paket pack output $PSScriptRoot\Bin\Packages buildconfig Debug buildplatform AnyCPU
+
+$url = "http://packages.ap.aderant.com/packages/"
+gci ".\bin\Packages\" -Filter *.nupkg | % { & $paket push file $_.FullName url $url apikey " " }
