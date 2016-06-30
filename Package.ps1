c8a42e5f7e816cd1b979536e316c56d5eff1c8fd
Michael Baker <michael.baker@aderant.com>
2016-07-01 09:37:53 +1200

    Removed test environment files as these are not needed in vNext

--- Package.ps1
+++ Package.ps1
@@ -1,8 +1,4 @@
-$paket = "$PSScriptRoot\Src\Build.Tools\paket.exe"
-
-& $paket restore
-
 & $paket pack output $PSScriptRoot\Bin\Packages buildconfig Debug buildplatform AnyCPU
 
 $url = "http://packages.ap.aderant.com/packages/"
-gci ".\bin\Packages\" -Filter *.nupkg | % { & $paket push file $_.FullName url $url apikey "abc" }
+#gci ".\bin\Packages\" -Filter *.nupkg | % { & $paket push file $_.FullName url $url apikey "abc" }
