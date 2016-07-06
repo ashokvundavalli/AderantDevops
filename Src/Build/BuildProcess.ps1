ffba34e0b9700a3cee1a7a9d0dd8d7ed37127de7
Michael Baker <michael.baker@aderant.com>
2016-07-06 21:14:49 +1200

    Add files that where excluded by .gitignore

---
+++ Src/Build/BuildProcess.ps1
@@ -0,0 +1,43 @@
+[CmdletBinding()]
+param(
+	$Configuration = 'Release',
+    $Repository
+)
+
+function Write-Info {
+    param ([string] $Message)
+
+    Write-Host "## $Message ##" -ForegroundColor Magenta
+}
+
+task Package Init, Clean, GetDependencies, Build, Test, {
+
+}
+
+task GetDependencies {
+    . $Env:EXPERT_BUILD_FOLDER\LoadDependencies.ps1 -modulesRootPath $Repository -dropPath "\\na.aderant.com\ExpertSuite\Main"
+}
+
+task Build {
+
+    exec {
+        MSBuild $Env:EXPERT_BUILD_FOLDER\ModuleBuild2.targets @$Repository\Build\TFSBuild.rsp /p:BuildRoot=$Repository
+    }
+
+}
+
+task Clean {    
+}
+
+task Test {
+}
+
+task Init {
+    Write-Info 'Establishing build properties'
+
+    $MSBuildLocation = ${Env:ProgramFiles(x86)} + "\MSBuild\$Env:VisualStudioVersion\Bin\"
+
+    use -Path $MSBuildLocation -Name MSBuild    
+}
+
+task Default Package
