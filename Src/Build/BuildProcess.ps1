007440ba456acd6c15b3c0bd7de8d539bcc2de30
Michael Baker <michael.baker@aderant.com>
2016-07-12 10:50:36 +1200

    Convert BuildProcess over to use an object for logging

--- Src/Build/BuildProcess.ps1
+++ Src/Build/BuildProcess.ps1
@@ -1,11 +1,69 @@
-[CmdletBinding()]
+#[CmdletBinding()]
 param(
-	$Configuration = 'Release',
+    $Configuration = 'Release',
     $Repository
 )
 
+Enum Result {
+ Succeeded
+ SucceededWithIssues
+ Failed
+ Cancelled
+ Skipped
+}
+
+Enum State {
+ Unknown
+ Initialized
+ InProgress
+ Completed
+}
+
+class LogDetail {
+    [Guid]$id = [Guid]::NewGuid()
+    [datetime]$startTime
+    [datetime]$finishTime
+    [State]$state
+    [Result]$result
+
+    LogDetail([string]$message){ 
+        $this.start($message)
+    }
+
+    [void] Start([string]$message) {
+        $this.startTime = [DateTime]::UtcNow
+        $this.state = [State]::InProgress
+        $this.Log($message)
+    }
+
+    [void] Finish([string]$message, [Result]$result) {   
+                
+        $this.finishTime = [DateTime]::UtcNow
+        $this.state = [State]::Completed
+        $this.result = $result
+        $this.Log($message)
+    }
+
+    [void] Log([string]$message) {
+        $stateText = $this.state.ToString()
+
+        if ($this.state -eq [State]::InProgress) {
+            Write-Host ("##vso[task.logdetail id=$($this.id);type=build;name=$message;order=1;starttime=$($this.startTime);state=$stateText;]")
+            return
+        }
+
+        if ($this.state -eq [State]::Completed) {
+            $resultText = $this.result.ToString()
+
+            Write-Host ("##vso[task.logdetail id=$($this.id);finishtime=$($this.finishTime);result=$resultText;state=$stateText]$message")        
+        }        
+    }  
+}
+
 $MSBuildLocation = ${Env:ProgramFiles(x86)} + "\MSBuild\14.0\Bin\"
 use -Path $MSBuildLocation -Name MSBuild
+
+$IsDesktopBuild = $Env:BUILD_URI -eq $null
 
 $dropLocation = "\\dfs.aderant.com\ExpertSuite\Dev\FrameworkNext"
 
@@ -15,37 +73,19 @@ function Write-Info {
     Write-Host "## $message ##" -ForegroundColor Magenta
 }
 
-function Write-Vso() {
-    param ([string] $message)
-    
-    #if ($Env:BUILD_URI) {
-        Write-Host $message
-    #}
-}
-
-function Start-BuildStep {
-    param ([string] $message)   
-    [string]$g = [Guid]::NewGuid()
-
-    Write-Vso "##vso[task.logdetail id=$g;name=project1;type=build;order=1]$message"     
-}
-
-function End-BuildStep {
-    param ([string] $message,
-    [ValidateSet('Succeeded','SucceededWithIssues','Failed','Cancelled','Skipped')] $state)
-
-    Write-Vso "##vso[task.complete result=$state;]$message"    
-}
-
-task Package -Jobs Init, Clean, GetDependencies, Build, Test, CopyToDrop, {
-    
+task Package -Jobs Init, Clean, GetDependencies, Build, Test, CopyToDrop, {        
 }
 
 task GetDependencies {    
-    #& $Env:EXPERT_BUILD_FOLDER\Build\LoadDependencies.ps1 -modulesRootPath $Repository -dropPath "\\na.aderant.com\ExpertSuite\Main"
+    $step = New-Object LogDetail "Get dependencies" 
+
+    & $Env:EXPERT_BUILD_FOLDER\Build\LoadDependencies.ps1 -modulesRootPath $Repository -dropPath "\\na.aderant.com\ExpertSuite\Main"
+
+    $step.Finish("Done", [Result]::Succeeded)
 }
 
 task Build {
+    $step = New-Object LogDetail "Build" 
 
     exec {        
         if ($Env:AGENT_HOMEDIRECTORY) {
@@ -53,9 +93,10 @@ task Build {
             $logger = "/dl:CentralLogger,`"$loggerAssembly`"*ForwardingLogger,`"$loggerAssembly`""
         }        
         
-        #MSBuild $Env:EXPERT_BUILD_FOLDER\Build\ModuleBuild2.targets @$Repository\Build\TFSBuild.rsp /p:BuildRoot=$Repository $logger
+        MSBuild $Env:EXPERT_BUILD_FOLDER\Build\ModuleBuild2.targets @$Repository\Build\TFSBuild.rsp /p:BuildRoot=$Repository $logger
     }
 
+     $step.Finish("Done", [Result]::Succeeded)
 }
 
 task Clean {    
@@ -64,8 +105,8 @@ task Clean {
 task Test {
 }
 
-task CopyToDrop {
-    Start-BuildStep "CopyToDrop"
+task CopyToDrop -If ($IsServerBuild) {
+    $step = New-Object LogDetail "CopyToDrop" 
 
     $text = Get-Content $Repository\Build\CommonAssemblyInfo.cs -Raw
     $text -match '(?m)(AssemblyFileVersion\(\")(?<version>[0-9]*.[0-9]*.[0-9]*.[0-9]*)' | Out-Null    
@@ -75,21 +116,24 @@ task CopyToDrop {
     $text -match 'ModuleName=(?<name>[^"]+)' | Out-Null    
     $name = $Matches.name    
     
-    #& $Env:EXPERT_BUILD_FOLDER\Build\CopyToDrop.ps1 -moduleRootPath $Repository -dropRootUNCPath $dropLocation\$name\1.8.0.0 -assemblyFileVersion $version
+    & $Env:EXPERT_BUILD_FOLDER\Build\CopyToDrop.ps1 -moduleRootPath $Repository -dropRootUNCPath $dropLocation\$name\1.8.0.0 -assemblyFileVersion $version
 
     $fullDropPath = "$dropLocation\$moduleName\1.8.0.0\$version"
 
     # Associate the drop back to the build
     Write-Vso "##vso[artifact.associate type=filepath;artifactname=drop]$fullDropPath"
 
-    End-BuildStep -message "CopyToDrop completed" -state Succeeded    
+    $step.Finish("Done", [Result]::Succeeded)
 }
 
 task Init {
-    Write-Info 'Establishing build properties'    
+    Write-Info "Build tree"
+    .\Show-BuildTree.ps1 -File $PSCommandPath
+
+    Write-Info "Establishing build properties"
 
     . $Env:EXPERT_BUILD_FOLDER\Build\Build-Libraries.ps1
-    CompileBuildLibraryAssembly $Env:EXPERT_BUILD_FOLDER\Build\
+    CompileBuildLibraryAssembly $Env:EXPERT_BUILD_FOLDER\Build\    
 }
 
 task Default Package
