918e249aab4a7bb96a0a8b971bbbc05df70accb0
Michael Baker <michael.baker@aderant.com>
2016-07-21 22:11:06 +1200

    Add framework for Expert build pipeline task

---
+++ Src/Sources/Aderant.Build.Agent/BuildPipeline/Pipeline.ps1
@@ -0,0 +1,14 @@
+﻿[CmdetBinding()]
+param
+(
+   [Parameter(Mandatory=$true)][string] $Repository,
+   [Parameter(Mandatory=$false)][string] $Version
+)
+
+$ErrorActionPreference = 'Stop'
+
+Trace-VstsEnteringInvocation $MyInvocation
+
+Write-Host $Repository
+Write-Host $Version
+Write-Host $distributedTaskContext
