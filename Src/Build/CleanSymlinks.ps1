2013d2250b37f09e9a73ee8b66c31d270d3d13fc
Michael Baker <michael.baker@aderant.com>
2016-08-01 14:47:19 +1200

    Clean up XML documentation. Clean up PowerShell syntax (formatting)

--- Src/Build/CleanSymlinks.ps1
+++ Src/Build/CleanSymlinks.ps1
@@ -1,9 +1,6 @@
-{
 [CmdletBinding()]
 Param ($path)
          
 Process {
     dir -Path $path -Force -Recurse -ErrorAction 'SilentlyContinue' | Where { $_.Attributes -match "ReparsePoint"} | % { [System.IO.Directory]::Delete($_.FullName) }
-}
-          
-}
+}        
