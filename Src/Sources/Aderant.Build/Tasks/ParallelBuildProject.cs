b883cbcdb5e294aded78d1ad4600e89d1bc0ddab
Michael Baker <michael.baker@aderant.com>
2016-08-09 10:05:08 +1200

    Add extra logging to ParallelBuildProjectFactory

--- Src/Sources/Aderant.Build/Tasks/ParallelBuildProject.cs
+++ Src/Sources/Aderant.Build/Tasks/ParallelBuildProject.cs
@@ -1,5 +1,7 @@
+using System;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Xml.Linq;
 using Aderant.Build.DependencyAnalyzer;
@@ -9,31 +11,38 @@ using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 namespace Aderant.Build.Tasks {
-
     public sealed class ParallelBuildProjectFactory : Task {
 
         [Required]
         public ITaskItem[] ModulesInBuild { get; set; }
 
         public string ModulesDirectory { get; set; }
-        
+
         public string ProductManifest { get; set; }
 
         public string ProjectFile { get; set; }
 
         public override bool Execute() {
-            ParallelBuildProjectController controller = new ParallelBuildProjectController();
-
-            ExpertManifest manifest = ExpertManifest.Load(ProductManifest);
-            manifest.ModulesDirectory = ModulesDirectory;
-
-            Log.LogMessage("Creating build project...");
-            Project project = controller.CreateProject(ModulesDirectory, manifest, ModulesInBuild.Select(m => Path.GetFileName(m.ItemSpec)));
-            XElement projectDocument = controller.CreateProjectDocument(project);
-
-            ParallelBuildProjectController.SaveBuildProject(Path.Combine(ModulesDirectory, ProjectFile), projectDocument);
-
+            Run();
             return !Log.HasLoggedErrors;
+        }
+
+        private void Run([CallerFilePath] string sourceFilePath = "") {
+            try {
+                ParallelBuildProjectController controller = new ParallelBuildProjectController();
+
+                ExpertManifest manifest = ExpertManifest.Load(ProductManifest);
+                manifest.ModulesDirectory = ModulesDirectory;
+
+                Log.LogMessage("Creating build project...");
+                Project project = controller.CreateProject(ModulesDirectory, manifest, ModulesInBuild.Select(m => Path.GetFileName(m.ItemSpec)));
+                XElement projectDocument = controller.CreateProjectDocument(project);
+
+                ParallelBuildProjectController.SaveBuildProject(Path.Combine(ModulesDirectory, ProjectFile), projectDocument);
+            } catch (Exception ex) {
+                Log.LogErrorFromException(ex, true, true, sourceFilePath);
+                throw;
+            }
         }
     }
 }
