23ba5785a247f0907429c7ebf874555021d4f18e
Michael Baker <michael.baker@aderant.com>
2016-08-09 15:34:16 +1200

    Enable kerberos for Git when setting up the profile

--- profilesetup.cmd
+++ profilesetup.cmd
@@ -1,4 +1,8 @@
 @echo off
 SET setupRoot=%~dp0
 powershell -noprofile -file %setupRoot:~0,-1%\Src\Profile\Aderant\Setup.ps1
+
+git config --global credential.tfs.integrated true
+git config --global core.autocrlf false
+
 pause
