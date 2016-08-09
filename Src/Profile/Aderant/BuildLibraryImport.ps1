41d530ea302f6b4ec5e77117fc2db5737d32e345
Michael Baker <michael.baker@aderant.com>
2016-08-09 15:29:07 +1200

    Add feature where the build system will only rebuild the DLL if your local version out of date vs head
    Related Work Items: #128143

--- Src/Profile/Aderant/BuildLibraryImport.ps1
+++ Src/Profile/Aderant/BuildLibraryImport.ps1
@@ -1,5 +1,16 @@
 ﻿$DebugPreference = 'SilentlyContinue'
 
+function SetUserEnvironmentVariableNoWait($name, $value) {
+    # SetEnvironmentVariable is very slow as it waits for apps to respond so we have this lovely async work around
+Add-Type -MemberDefinition @"
+public static void SetUserEnvironmentVariableNoWait(string name, string value) {
+    System.Threading.ThreadPool.QueueUserWorkItem((_) => System.Environment.SetEnvironmentVariable(name, value, System.EnvironmentVariableTarget.User));
+}
+"@ -Name Internal -NameSpace System -UsingNamespace System.Threading
+
+    [Internal]::SetUserEnvironmentVariableNoWait($name, $value)
+}
+
 function BuildProject($properties, [bool]$rebuild) {
     # Load the build libraries as this has our shared compile function. This function is shared by the desktop and server bootstrap of Build.Infrastructure
     $buildScripts = $properties.BuildScriptsDirectory
@@ -41,37 +52,31 @@ function LoadAssembly($properties, [string]$targetAssembly) {
     }
 }
 
-function UpdateOrBuildAssembly($properties) {
-    Write-Debug "Profile home: $actualPath"
+function UpdateOrBuildAssembly($properties) {    
     $aderantBuildAssembly = [System.IO.Path]::Combine($properties.BuildToolsDirectory, "Aderant.Build.dll")	
-
-    $needToBuild = $false
     
     if (-not [System.IO.File]::Exists($aderantBuildAssembly)) {
         Write-Host "No Aderant.Build.dll found at $aderantBuildAssembly. Creating..."
-        $needToBuild = $true
-    }
-
-    if ($needToBuild -eq $true) {
-        Write-Host "Building Build.Infrastructure..."
         BuildProject $properties $true
-    }
+    }    
 
-    # Test if one of the files is older than a day
-    $aderantBuildFileInfo = Get-ChildItem $aderantBuildAssembly	
+    $outdatedAderantBuildFile = $false
 
-    $outdatedAderantBuildFile = $false	
-    
-    $dt = $aderantBuildFileInfo.LastWriteTime.ToString("d", [System.Globalization.CultureInfo]::CurrentCulture)
-    if ($aderantBuildFileInfo.LastWriteTime.Date -le [System.DateTime]::Now.AddDays(-1)) {
-        Write-Host ("Aderant.Build.dll is out of date ({0}). Updating..." -f $dt)
+    pushd $PSScriptRoot
+    [string]$head = & git rev-parse HEAD
+    popd   
+
+    Write-Host "Version: $head"
+
+    $version = $Env:EXPERT_BUILD_VERSION
+
+    if ($version -ne $head) {        
         $outdatedAderantBuildFile = $true
-    } else {
-        Write-Host ("Aderant.Build.dll is not out of date. {0} is less than 1 day old" -f $dt)
     }
 
     if ($outdatedAderantBuildFile) {
         BuildProject $properties $true
+        SetUserEnvironmentVariableNoWait "EXPERT_BUILD_VERSION" $head        
     }
 
     # Now actually load Aderant.Build.dll
