701b180f4352cc3273b215e8018a4f74b6760bce
Michael Baker <michael.baker@aderant.com>
2017-05-08 11:55:48 +1200

    PR 3459: Introduce free space check.
    Introduce free space check.
    
    This ensures that the build system explodes if there is less than 10 GB available.

---
+++ Src/Sources/Aderant.Build/Tasks/CheckFreeSpace.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Aderant.Build.Tasks {
+    public sealed class CheckFreeSpace : Task {
+        [Required]
+        public int FreeSpace { get; set; }
+
+        [Required]
+        public string Units { get; set; }
+
+        public override bool Execute() {
+            if (string.Equals("GB", Units, StringComparison.OrdinalIgnoreCase)) {
+                long freeSizeInBytes = (long)FreeSpace * 1024 * 1024 * 1024;
+
+                DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+                foreach (DriveInfo d in allDrives) {
+                    if (d.IsReady) {
+                        if (d.DriveType == DriveType.Fixed) {
+                            if (d.AvailableFreeSpace < freeSizeInBytes) {
+                                Log.LogError($"Available free space on drive {d.Name} is below minimum requirement of {freeSizeInBytes / 1024 / 1024 / 1024} GB for a reliable build.");
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return !Log.HasLoggedErrors;
+            }
+
+            throw new ArgumentOutOfRangeException("Only GB is supported as the unit", Units);
+        }
+    }
+}
