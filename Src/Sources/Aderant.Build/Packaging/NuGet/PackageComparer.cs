f068ce91bb9636a25751688ffa9bc36a90dfa0ad
Jamie Smillie <jamie.smillie@aderant.com>
2017-11-08 15:17:51 +1300

    Merged PR 11274: Updated Package Comparer to account for changes in directory structure
    Updated Package Comparer to account for changes in directory structure
    
    Related work items: #181583

--- Src/Sources/Aderant.Build/Packaging/NuGet/PackageComparer.cs
+++ Src/Sources/Aderant.Build/Packaging/NuGet/PackageComparer.cs
@@ -25,26 +25,33 @@ namespace Aderant.Build.Packaging.NuGet {
 
             logger.Info("Existing package directory: {0}. Current package directory: {1}", existingPackageDirectory, currentPackageDirectory);
             
-            IEnumerable<string> currentContents = fileSystem.GetFiles(currentPackageDirectory, "*", true).ToList();
-            IEnumerable<string> existingContents = fileSystem.GetFiles(existingPackageDirectory, "*", true).ToList();
+            IEnumerable<string> currentContents = fileSystem.GetFiles(currentPackageDirectory, "*", true, true).ToList();
+            IEnumerable<string> existingContents = fileSystem.GetFiles(existingPackageDirectory, "*", true, true).ToList();
 
             Dictionary<string, string> currentHashes = HashContents(currentContents);
             Dictionary<string, string> existingHashes = HashContents(existingContents);
 
             IEnumerable<string> difference = currentHashes.Values.Except(existingHashes.Values);
 
-            foreach (var item in difference) {
+            foreach (string item in difference) {
                 string myKey = currentHashes.FirstOrDefault(x => x.Value == item).Key;
-                logger.Info("New or changed item found: {0}", myKey);
+                logger.Info("New or changed item found: {0}", Path.GetFileName(myKey));
             }
 
-            return difference.Any();
+            HashSet<string> currentFileLocations = new HashSet<string>(currentContents.Select(x => x.Replace($@"{currentPackageDirectory}\", "")), StringComparer.OrdinalIgnoreCase);
+            HashSet<string> existingFileLocations = new HashSet<string>(existingContents.Select(x => x.Replace($@"{existingPackageDirectory}\", "")), StringComparer.OrdinalIgnoreCase);
+
+            if (!currentFileLocations.SetEquals(existingFileLocations)) {
+                logger.Info("Package structure has changed.");
+            }
+
+            return !currentFileLocations.SetEquals(existingFileLocations) || difference.Any();
         }
 
         private Dictionary<string, string> HashContents(IEnumerable<string> files) {
             Dictionary<string, string> hashes = new Dictionary<string, string>();
 
-            foreach (var file in files) {
+            foreach (string file in files) {
                 string hashValue;
 
                 using (Stream stream = fileSystem.OpenFile(file)) {
