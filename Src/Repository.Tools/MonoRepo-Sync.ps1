0008d7d498bd0b6774b327bebdf8e4b7f1a22576
Lucas Ying <lucas.ying@aderant.com>
2018-03-06 14:22:09 +1300

    Merged PR 15581: Optimized monorepo sync script, it could sync changes, file creates, file
    Optimized monorepo sync script, it could sync changes, file creates, file deletes from all original repos now.
    
    Related work items: #188122, #188398

--- Src/Repository.Tools/MonoRepo-Sync.ps1
+++ Src/Repository.Tools/MonoRepo-Sync.ps1
@@ -1,6 +1,6 @@
 # Metatool for combining the multiple repositories that the ExpertSuite uses.
 param (
-    [Parameter(Mandatory=$false)][array]$repos=@("AccountsPayable", "AderantExpertLauncher", "Billing", "Budgeting", "Build.Infrastructure", "Case", "ClientMatter", "Conflicts", "Customization", "Database", "Deployment", "Disbursements", "Framework", "Inquiries", "MatterPlanning", "Presentation"),
+    [Parameter(Mandatory=$false)][array]$repos=@("AccountsPayable", "AderantExpertLauncher", "Billing", "Budgeting", "Build.Infrastructure", "Case", "ClientMatter", "Conflicts", "Customization", "Database", "Deployment", "Disbursements", "Framework", "Inquiries", "MatterPlanning", "Presentation", "UIAutomation.Framework"),
     [Parameter(Mandatory=$false)][string]$branch="master"
 )
 
@@ -20,29 +20,20 @@ foreach ($repo in $repos){
             git subtree pull --prefix $repo $repo $branch --squash
             Write-Host "Pull $repo/$branch completed."
         } else {
-            git pull $repo $branch
+            git subtree pull --prefix $repo $repo $branch
             Write-Host "Pull $repo/$branch completed."
-            dir .\ | ? {$_.Name -notin $exclude} | %{git mv $_.Name $repo}
-            git commit -m "Merge $repo"
         }
     } else {
         git remote add -f $repo "$baseUrl/$repo"
         Write-Host "Add remote $repo done."
-        git fetch $repo
 
         # This guy has a messed up history graph so we treat it as a special case. The history of objects from this won't have the same fidelity as the rest of the merged repositories.
         if ($repo -eq "Disbursements") {
             git subtree add --prefix $repo $repo $branch --squash
             git subtree pull --prefix $repo $repo $branch --squash
         } else {
-            git merge -m "Merging $repo/$branch ..." "$repo/$branch" --allow-unrelated-histories
-            Write-Host "Merge $repo/$branch done."
-
-            mkdir $repo
-            Write-Host "make dir $repo done."
-            dir .\ | ? {$_.Name -notin $exclude} | %{git mv $_.Name $repo}
-
-            git commit -m "Merge $repo into MonoRepo"
+            git subtree add --prefix $repo $repo $branch
+            git subtree pull --prefix $repo $repo $branch
         }
     }
 }
