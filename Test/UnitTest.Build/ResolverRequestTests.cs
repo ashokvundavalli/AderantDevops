3777a9ba37b287be4d4f53b6a30f704f0aeb2d23
Lucas Ying <lucas.ying@aderant.com>
2018-03-12 14:10:15 +1300

    Fixed unit tests against DependencyRequirement.Create method change

--- Test/UnitTest.Build/ResolverRequestTests.cs
+++ Test/UnitTest.Build/ResolverRequestTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Aderant.Build;
 using Aderant.Build.DependencyAnalyzer;
 using Aderant.Build.DependencyResolver;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -30,7 +31,7 @@ namespace UnitTest.Build {
 
             request.SetDependenciesDirectory("Baz");
 
-            var actual = request.GetDependenciesDirectory(DependencyRequirement.Create("Foo"));
+            var actual = request.GetDependenciesDirectory(DependencyRequirement.Create("Foo", BuildConstants.MainDependencyGroup));
 
             Assert.AreEqual("Baz", actual);
         }
@@ -41,7 +42,7 @@ namespace UnitTest.Build {
 
             var request = new ResolverRequest(null, "Foo", fooModule, new ExpertModule { Name = "Bar" });
 
-            var barDependency = DependencyRequirement.Create("Bar");
+            var barDependency = DependencyRequirement.Create("Bar", BuildConstants.MainDependencyGroup);
 
             request.AssociateRequirements(fooModule, new[] { barDependency });
 
@@ -52,7 +53,7 @@ namespace UnitTest.Build {
 
         [TestMethod]
         public void ResolverRequest_NoExistingDependency() {
-            IDependencyRequirement requirement = DependencyRequirement.Create("a");
+            IDependencyRequirement requirement = DependencyRequirement.Create("a", BuildConstants.MainDependencyGroup);
             var resolverRequest = new ResolverRequest(null, "Foo");
             Assert.AreEqual(0, resolverRequest.dependencies.Count);
             var result = resolverRequest.GetOrAdd(requirement);
@@ -62,7 +63,7 @@ namespace UnitTest.Build {
 
         [TestMethod]
         public void ResolverRequest_ExistingDependency() {
-            IDependencyRequirement requirement = DependencyRequirement.Create("a");
+            IDependencyRequirement requirement = DependencyRequirement.Create("a", BuildConstants.MainDependencyGroup);
             var resolverRequest = new ResolverRequest(null, "Foo");
             Assert.AreEqual(0, resolverRequest.dependencies.Count);
 
@@ -76,8 +77,8 @@ namespace UnitTest.Build {
         [TestMethod]
         public void ResolverRequest_UniqueDependencies() {
             IDependencyRequirement[] requirements = {
-                DependencyRequirement.Create("a"),
-                DependencyRequirement.Create("b")
+                DependencyRequirement.Create("a", BuildConstants.MainDependencyGroup),
+                DependencyRequirement.Create("b", BuildConstants.MainDependencyGroup)
             };
 
             var resolverRequest = new ResolverRequest(null, "Foo");
