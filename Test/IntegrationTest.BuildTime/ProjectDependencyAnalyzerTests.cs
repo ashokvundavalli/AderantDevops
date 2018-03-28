300f2f97088e85ec78379d300e0bc83db938d54b
Jamie Smillie <jamie.smillie@aderant.com>
2018-03-28 14:22:03 +1300

    Added integration test project

---
+++ Test/IntegrationTest.BuildTime/ProjectDependencyAnalyzerTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTest.BuildTime {
+    [TestClass]
+    public class ProjectDependencyAnalyzerTests {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
