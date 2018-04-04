78be9713b4795b57c56e268413b895cb5ed6182c
Jamie Smillie <jamie.smillie@aderant.com>
2018-04-05 10:51:29 +1200

    Added asserts for dependency order

--- Test/IntegrationTest.BuildTime/ProjectDependencyAnalyzerTests.cs
+++ Test/IntegrationTest.BuildTime/ProjectDependencyAnalyzerTests.cs
@@ -26,7 +26,13 @@ namespace IntegrationTest.BuildTime {
             ProjectDependencyAnalyzer projectDependencyAnalyzer = new ProjectDependencyAnalyzer(new CSharpProjectLoader(), new TextTemplateAnalyzer(fileSystem), fileSystem);
 
             List<IDependencyRef> dependencyOrder = projectDependencyAnalyzer.GetDependencyOrder(new AnalyzerContext().AddDirectory(sourceDirectory));
-            
+
+            Assert.AreEqual("Module.Initialize", dependencyOrder[0].Name);
+            Assert.AreEqual("Project1", dependencyOrder[1].Name);
+            Assert.AreEqual("Project3", dependencyOrder[2].Name);
+            Assert.AreEqual("Project2", dependencyOrder[3].Name);
+            Assert.AreEqual("Module.Completion", dependencyOrder[4].Name);
+
             // Assert dependency order
             Assert.IsTrue(File.Exists(Path.Combine(sourceDirectory, "DependencyGraph.txt")));
         }
