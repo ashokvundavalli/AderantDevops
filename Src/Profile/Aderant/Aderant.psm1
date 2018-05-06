101708b26936022ad5b00ccdbe1f379b15eed0e9
Jamie Smillie <jamie.smillie@aderant.com>
2018-05-07 11:19:48 +1200

    Add Binaries directory creation in the event it does not exist for Get-ProductZip

--- Src/Profile/Aderant/Aderant.psm1
+++ Src/Profile/Aderant/Aderant.psm1
@@ -2267,7 +2267,7 @@ function Generate-SystemMap() {
     if ($systemMapConnectionString.ToLower().Contains("/pdbs:") -and $systemMapConnectionString.ToLower().Contains("/pdbd:") -and $systemMapConnectionString.ToLower().Contains("/pdbid:") -and $systemMapConnectionString.ToLower().Contains("/pdbpw:")) {
         $connectionParts = $systemMapConnectionString.Split(" ")
         Write-Debug "Connection is [$connectionParts]"
-        &$inDirectory\Systemmapbuilder.exe /f:$inDirectory /o:$inDirectory\systemmap.xml /ef:Customization $connectionParts[0] $connectionParts[1] $connectionParts[2] $connectionParts[3]
+        & $inDirectory\Systemmapbuilder.exe /f:$inDirectory /o:$inDirectory\systemmap.xml /ef:Customization $connectionParts[0] $connectionParts[1] $connectionParts[2] $connectionParts[3]
     } else {
         Write-Error "Connection string is invalid for use with systemmapbuilder.exe [$systemMapConnectionString]"
     }
