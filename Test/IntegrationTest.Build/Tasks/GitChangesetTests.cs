0e4940a740e0e002db2070c02f7e76cb5310577c
Lucas Ying <lucas.ying@aderant.com>
2018-05-18 09:26:30 +1200

    retrieve changed files in commits from parent branch diverged

--- Test/IntegrationTest.Build/Tasks/GitChangesetTests.cs
+++ Test/IntegrationTest.Build/Tasks/GitChangesetTests.cs
@@ -1,24 +1,31 @@
-using System.IO;
+﻿using System.IO;
 using Aderant.Build.Tasks;
+using Aderant.BuildTime.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IntegrationTest.Build.Tasks {
     [TestClass]
     [DeploymentItem("Aderant.BuildTime.Tasks.dll")]
-    public class GitVersionTests : BuildTaskTestBase {
+    public class GitChangesetTests : BuildTaskTestBase {
         [TestInitialize]
         public void NativeLibraryAvailable() {
-            var foo = typeof(GitVersion);
+            var foo = typeof(GitChangeset);
             string nativeLibraryPath = LibGit2Sharp.GlobalSettings.NativeLibraryPath;
 
             Assert.IsTrue(Directory.Exists(nativeLibraryPath));
         }
 
         [TestMethod]
-        public void GitVersion_runs_without_exception() {
-            RunTarget("GitVersion");
+        public void GitChangeset_runs_without_exception() {
+            RunTarget("GitChangeset");
 
             Assert.IsFalse(Logger.HasRaisedErrors);
+        }
+
+        [TestMethod]
+        public void GitChangesetExecuteTest() {
+            var changeset = new GitChangeset() {WorkingDirectory = @"C:\Git\Deployment" };
+            changeset.Execute();
         }
     }
 }
