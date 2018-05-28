a31a717ff37af5ebf8cdb36aac32391e863082de
Lucas Ying <lucas.ying@aderant.com>
2018-05-29 11:10:17 +1200

    fixed paramater name

--- Src/Profile/Aderant/Functions/Get-AmbientBuildMetadata.ps1
+++ Src/Profile/Aderant/Functions/Get-AmbientBuildMetadata.ps1
@@ -36,7 +36,7 @@ function Get-AmbientBuildMetadata
         $buildInfo.ScmCommitId = Get-EnvironmentVariable 'GIT_COMMIT'
         $buildInfo.ScmBranch = Get-EnvironmentVariable 'BUILD_SOURCEBRANCH'
 
-        $build.SetPullRequestInfo(
+        $buildInfo.SetPullRequestInfo(
             (Get-EnvironmentVariable 'SYSTEM_PULLREQUEST_PULLREQUESTID'),
             (Get-EnvironmentVariable 'SYSTEM_PULLREQUEST_SOURCEBRANCH'),
             (Get-EnvironmentVariable 'SYSTEM_PULLREQUEST_TARGETBRANCH'))
