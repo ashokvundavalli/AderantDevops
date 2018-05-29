a9673b9790f2b2a496a38feb9900decb73bdc8aa
Lucas Ying <lucas.ying@aderant.com>
2018-05-30 09:38:39 +1200

    work in progress

--- Src/Profile/Aderant/Aderant.psm1
+++ Src/Profile/Aderant/Aderant.psm1
@@ -6,7 +6,8 @@ Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Functions') -Filt
 ForEach-Object { . $_.FullName }
 
 function InitializePrivateData {    
-    $context = (New-BuildContext -Environment "AutoDiscover")
+    $context = [Aderant.Build.Context]::new()
+    #$context = (New-BuildContext -Environment "AutoDiscover")
     Write-Debug ($context | Out-String)
     $MyInvocation.MyCommand.Module.PrivateData.Context = $context
 }
