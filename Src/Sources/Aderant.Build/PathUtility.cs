c4d8b7b05cc99f2e4496f71e638894361a619cb8
Michael Baker <michael.baker@aderant.com>
2018-06-25 11:23:28 +1200

    Relative path unit tests

--- Src/Sources/Aderant.Build/PathUtility.cs
+++ Src/Sources/Aderant.Build/PathUtility.cs
@@ -1,8 +1,11 @@
-using System;
+﻿using System;
 using System.IO;
 
 namespace Aderant.Build {
     public static class PathUtility {
+
+        internal static string DirectorySeparatorChar = Path.DirectorySeparatorChar.ToString();
+
         public static string GetPathWithForwardSlashes(string path) {
             return path.Replace('\\', '/');
         }
@@ -17,10 +20,14 @@ namespace Aderant.Build {
             }
 
             // if the path is empty, we want to return the original string instead of a single trailing character.
-            if (path.Length == 0
-                || path[path.Length - 1] == trailingCharacter) {
+            if (path.Length == 0 || path[path.Length - 1] == trailingCharacter) {
                 return path;
             }
+
+            // Optimization: Avoid boxing trailingCharacter
+            if (trailingCharacter == Path.DirectorySeparatorChar) {
+                return path + DirectorySeparatorChar;
+            } 
 
             return path + trailingCharacter;
         }
@@ -33,18 +40,40 @@ namespace Aderant.Build {
             return ends + text + ends;
         }
 
+        /// <summary>
+        /// Given the absolute location of a file, and a disc location, returns relative file path to that disk location. 
+        /// Throws UriFormatException.
+        /// </summary>
+        /// <param name="basePath">
+        /// The base path we want to be relative to. Must be absolute.  
+        /// Should <i>not</i> include a filename as the last segment will be interpreted as a directory.
+        /// </param>
+        /// <param name="path">
+        /// The path we need to make relative to basePath.  The path can be either absolute path or a relative path in which case it is relative to the base path.
+        /// If the path cannot be made relative to the base path (for example, it is on another drive), it is returned verbatim.
+        /// If the basePath is an empty string, returns the path.
+        /// </param>
+        /// <returns>relative path (can be the full path)</returns>
         public static string MakeRelative(string basePath, string path) {
             if (basePath.Length == 0) {
                 return path;
             }
-            Uri uri = new Uri(EnsureTrailingSlash(basePath), UriKind.Absolute);
-            Uri uri2 = CreateUriFromPath(path);
-            if (!uri2.IsAbsoluteUri) {
-                uri2 = new Uri(uri, uri2);
+
+            Uri baseUri = new Uri(EnsureTrailingSlash(basePath), UriKind.Absolute); // May throw UriFormatException
+
+            Uri pathUri = CreateUriFromPath(path);
+
+            if (!pathUri.IsAbsoluteUri) {
+                // the path is already a relative url, we will just normalize it...
+                pathUri = new Uri(baseUri, pathUri);
             }
-            Uri uri3 = uri.MakeRelativeUri(uri2);
-            string text = Uri.UnescapeDataString(uri3.IsAbsoluteUri ? uri3.LocalPath : uri3.ToString());
-            return text.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+
+            Uri relativeUri = baseUri.MakeRelativeUri(pathUri);
+            string relativePath = Uri.UnescapeDataString(relativeUri.IsAbsoluteUri ? relativeUri.LocalPath : relativeUri.ToString());
+
+            string result = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+
+            return result;
         }
 
         private static Uri CreateUriFromPath(string path) {
---
+++ Test/UnitTest.Build/PathUtilityTests.cs
@@ -0,0 +1,23 @@
+﻿using Aderant.Build;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Build {
+    [TestClass]
+    public class PathUtilityTests {
+
+        [TestMethod]
+        public void MakeRelativeTests() {
+            Assert.AreEqual(@"foo.cpp", PathUtility.MakeRelative(@"c:\abc\def", @"c:\abc\def\foo.cpp"));
+            Assert.AreEqual(@"def\foo.cpp", PathUtility.MakeRelative(@"c:\abc\", @"c:\abc\def\foo.cpp"));
+            Assert.AreEqual(@"..\foo.cpp", PathUtility.MakeRelative(@"c:\abc\def\xyz", @"c:\abc\def\foo.cpp"));
+            Assert.AreEqual(@"..\ttt\foo.cpp", PathUtility.MakeRelative(@"c:\abc\def\xyz\", @"c:\abc\def\ttt\foo.cpp"));
+            Assert.AreEqual(@"e:\abc\def\foo.cpp", PathUtility.MakeRelative(@"c:\abc\def", @"e:\abc\def\foo.cpp"));
+            Assert.AreEqual(@"foo.cpp", PathUtility.MakeRelative(@"\\aaa\abc\def", @"\\aaa\abc\def\foo.cpp"));
+            Assert.AreEqual(@"foo.cpp", PathUtility.MakeRelative(@"c:\abc\def", @"foo.cpp"));
+            Assert.AreEqual(@"foo.cpp", PathUtility.MakeRelative(@"c:\abc\def", @"..\def\foo.cpp"));
+            Assert.AreEqual(@"\\host\path\file", PathUtility.MakeRelative(@"c:\abc\def", @"\\host\path\file"));
+            Assert.AreEqual(@"\\host\d$\file", PathUtility.MakeRelative(@"c:\abc\def", @"\\host\d$\file"));
+            Assert.AreEqual(@"..\fff\ggg.hh", PathUtility.MakeRelative(@"c:\foo\bar\..\abc\cde", @"c:\foo\bar\..\abc\fff\ggg.hh"));
+        }
+    }
+}
