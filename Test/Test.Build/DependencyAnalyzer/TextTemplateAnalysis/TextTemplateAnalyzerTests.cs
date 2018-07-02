8db839e1f3a018261e293601f775dea636db6515
Michael Baker <michael.baker@aderant.com>
2018-07-03 11:12:11 +1200

    Add tests for text template analysis

---
+++ Test/Test.Build/DependencyAnalyzer/TextTemplateAnalysis/TextTemplateAnalyzerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Linq;
+using Aderant.Build.DependencyAnalyzer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTest.Build.DependencyAnalyzer.TextTemplateAnalysis {
+
+    [TestClass]
+    public class TextTemplateAnalyzerTests {
+
+        [TestMethod]
+        public void Extract_assembly_reference() {
+
+            var analyzer = new TextTemplateAnalyzer();
+
+            TextTemplateAnalysisResult result = analyzer.Analyze(new StringReader(Resources.SimpleTextTemplate));
+
+            Assert.AreEqual(1, result.AssemblyReferences.Count);
+            Assert.AreEqual("System.Core", result.AssemblyReferences.First());
+        }
+    }
+}
