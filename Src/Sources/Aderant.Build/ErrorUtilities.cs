1959a716ecf168f1b3b9f7de3bc83732d8e13f05
Michael Baker <michael.baker@aderant.com>
2018-07-03 14:45:43 +1200

    Add global error util class for precondition checks

---
+++ Src/Sources/Aderant.Build/ErrorUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using Aderant.Build.Annotations;
+
+namespace Aderant.Build {
+    internal static class ErrorUtilities {
+
+        [AssertionMethod]
+        [DebuggerStepThrough]
+        public static void IsNotNull<T>(
+            [AssertionCondition(AssertionConditionType.IS_NOT_NULL)]
+            T obj,
+            string name) where T : class {
+            if (obj == null) {
+                throw new ArgumentNullException(name);
+            }
+        }
+    }
+}
