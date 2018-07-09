ad07f81161777efff9f17f580fe2bd61a11d53d5
Michael Baker <michael.baker@aderant.com>
2018-07-09 15:29:09 +1200

    Remove code moved to another file

--- Src/Sources/Aderant.Build/DependencyAnalyzer/Model/DirectoryNode.cs
+++ Src/Sources/Aderant.Build/DependencyAnalyzer/Model/DirectoryNode.cs
@@ -1,37 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using Aderant.Build.Model;
-using Aderant.Build.ProjectSystem.References;
 
 namespace Aderant.Build.DependencyAnalyzer.Model {
-    public abstract class AbstractArtifact : IArtifact {
-        private List<IResolvedDependency> resolvedDependencies = new List<IResolvedDependency>();
-        private List<IUnresolvedDependency> unresolvedDependencies = new List<IUnresolvedDependency>();
-
-        public virtual IReadOnlyCollection<IDependable> GetDependencies() {
-            return resolvedDependencies.Select(d => d.ResolvedReference).ToList();
-        }
-
-        public virtual void AddResolvedDependency(IUnresolvedDependency unresolvedDependency, IDependable dependable) {
-            IResolvedDependency resolvedDependency;
-
-            if (unresolvedDependency == null) {
-                resolvedDependency = ResolvedDependency.Create(this, dependable);
-            } else {
-                resolvedDependency = ResolvedDependency.Create(this, dependable, unresolvedDependency);
-            }
-
-            resolvedDependencies.Add(resolvedDependency);
-
-            if (unresolvedDependency != null) {
-                unresolvedDependencies.Remove(unresolvedDependency);
-            }
-        }
-
-        public abstract string Id { get; }
-    }
 
     [DebuggerDisplay("DirectoryNode: {" + nameof(Id) + "}")]
     internal sealed class DirectoryNode : AbstractArtifact {
