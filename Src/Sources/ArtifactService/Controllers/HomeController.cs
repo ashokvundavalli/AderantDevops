473a5606ff32ad403be24c06ac6439d8688c5775
Michael Baker <michael.baker@aderant.com>
2018-07-30 20:23:07 +1200

    WebAPI, MVC, OWIN it's all so confusing

--- Src/Sources/ArtifactService/Controllers/HomeController.cs
+++ Src/Sources/ArtifactService/Controllers/HomeController.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace ArtifactService.Controllers {
+
+    [RoutePrefix("api/v1/home")]
     public class HomeController : ApiController {
 
-        // GET api/values 
+        [Route("")]
+        [HttpGet]
         public IEnumerable<string> Get() {
             return new string[] { "value1", "value2" };
+        }
+
+        [Route("showlist")]
+        [HttpGet]
+        public IHttpActionResult ShowList(string id) {
+            return Ok(new string[] { "value1", "value2" });
         }
     }
 }
--- Src/Sources/ArtifactService/Startup.cs
+++ Src/Sources/ArtifactService/Startup.cs
@@ -1,35 +1,65 @@
 ﻿using System;
 using System.Net.Http.Formatting;
 using System.Web.Http;
+using ArtifactService;
 using Microsoft.Owin;
-
+using Microsoft.Owin.Extensions;
+using Microsoft.Owin.Hosting;
 using Owin;
 
-[assembly: OwinStartup(typeof(ArtifactService.Startup))]
+[assembly: OwinStartup(typeof(Startup))]
 
 namespace ArtifactService {
-    public class Startup {
-        public void Configuration(IAppBuilder app) {
 
-            // Configure Web API for self-host. 
-            HttpConfiguration config = new HttpConfiguration();
+    public static class WebApiConfig {
+        public static void Register(HttpConfiguration config) {
+            // Attribute routing.
+            //config.MapHttpAttributeRoutes();
+
+            // Convention-based routing.
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+    }
+
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            //// Configure Web API for self-host. 
+            //HttpConfiguration config = new HttpConfiguration();
+            //app.UseWebApi(config);
+
+            //WebApiConfig.Register(config);
+            app.Use((context, next) => {
+                context.Response.Headers.Remove("Server");
+                return next.Invoke();
+            });
+            app.UseStageMarker(PipelineStage.PostAcquireState);
+
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
 
             config.Formatters.Clear();
             config.Formatters.Add(new JsonMediaTypeFormatter());
 
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
             app.UseWebApi(config);
         }
     }
+    
 
     class Program {
 
         static void Main(string[] args) {
-            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:9000")) {
+            using (WebApp.Start<Startup>("http://localhost:9000")) {
                 Console.WriteLine("Press [enter] to quit...");
                 Console.ReadLine();
             }
