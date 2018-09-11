0e757c07644188aeb75389436777fb1d20af5092
Michael Baker <michael.baker@aderant.com>
2018-09-11 13:55:12 +1200

    Try throwing from finally

--- Src/Build/Functions/Invoke-Build.ps1
+++ Src/Build/Functions/Invoke-Build.ps1
@@ -287,32 +287,34 @@ Should not be used as it prevents incremental builds which increases build times
     } finally {
         $context = $contextService.CurrentContext
 
+        $reason = $context.BuildStatusReason
+        $status = $context.BuildStatus
+
+        Write-Output ""
+        Write-Output ""
+
+        Write-Host " Build: " -NoNewline
+
+        if (-not $succeded -or $context.BuildStatus -eq "Failed") {
+            Write-Host "[" -NoNewline
+            Write-Host ($status.ToUpper()) -NoNewline -ForegroundColor Red
+            Write-Host "]"
+            Write-Host " $reason" -ForegroundColor Red
+
+            if (-not $context.IsDesktopBuild) {
+                throw "Build did not succeed: $($context.BuildStatusReason)"
+            }
+        } else {            
+            Write-Host "[" -NoNewline
+            Write-Host ($status.ToUpper()) -NoNewline -ForegroundColor Green
+            Write-Host "]"
+            Write-Host " $reason" -ForegroundColor Gray    
+        }
+
         if ($contextService -ne $null) {
             $contextService.Dispose()
         }
     }
    
-    $reason = $context.BuildStatusReason
-    $status = $context.BuildStatus
-
-    Write-Output ""
-    Write-Output ""
-
-    Write-Host " Build: " -NoNewline
-
-    if ($context.BuildStatus -eq "Failed") {
-        Write-Host "[" -NoNewline
-        Write-Host ($status.ToUpper()) -NoNewline -ForegroundColor Red
-        Write-Host "]"
-        Write-Host " $reason" -ForegroundColor Red
-
-        if (-not $context.IsDesktopBuild) {
-            throw "Build did not succeed: $($context.BuildStatusReason)"
-        }
-    } else {            
-        Write-Host "[" -NoNewline
-        Write-Host ($status.ToUpper()) -NoNewline -ForegroundColor Green
-        Write-Host "]"
-        Write-Host " $reason" -ForegroundColor Gray    
-    }
+   
 }
