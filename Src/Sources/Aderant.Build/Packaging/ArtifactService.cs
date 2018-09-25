c24f751f786659904a06e5c3b5146ea509e48c0e
Jamie Smillie <jamie.smillie@aderant.com>
2018-09-26 10:41:47 +1200

    Added solution root to output path

--- Src/Sources/Aderant.Build/Packaging/ArtifactService.cs
+++ Src/Sources/Aderant.Build/Packaging/ArtifactService.cs
@@ -384,7 +384,7 @@ namespace Aderant.Build.Packaging {
                                 logger.Warning($"File {filePath} exists in more than one artifact." + Environment.NewLine + duplicates);
                             }
 
-                            string destination = Path.GetFullPath(Path.Combine(project.Value.OutputPath, filePath));
+                            string destination = Path.GetFullPath(Path.Combine(solutionRoot, project.Value.OutputPath, filePath));
 
                             if (destinationPaths.Add(destination)) {
                                 logger.Info($"Selected artifact: {selectedArtifact}");
