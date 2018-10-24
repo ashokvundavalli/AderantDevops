e22304954a985c3180c7d522cdcae11d4a71a649
Terence Le Grove Smith <terence.legrovesmith@aderant.com>
2018-10-25 11:35:51 +1300

    PR 21628: Created a rule which prevents one from using interfaces in our repository (NHibernate). Not active
    Created a rule which prevents one from using interfaces in our repository (NHibernate).
    This rule is not active as it is no longer required. Code is submitted as an example for reference purposes.

---
+++ Src/Sources/Aderant.Build.Analyzer/Rules/NHibernateRetrieveTypeNotInterfaceRule.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Aderant.Build.Analyzer.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Aderant.Build.Analyzer.Rules {
+    /// <summary>
+    /// This rule makes sure that the type parameters in IQuery.Lynq<> and IQuery.Get<> are classes instead of interfaces.
+    /// The bug that prompted me to develop this rule has since been fixed so it is here mostly as an example.
+    /// </summary>
+    internal class NHibernateRetrieveTypeNotInterfaceRule : RuleBase {
+        internal const string DiagnosticId = "Aderant_NHibernateDoesNotSupportInterface";
+
+        internal override DiagnosticSeverity Severity => DiagnosticSeverity.Error;
+        internal override string Id => DiagnosticId;
+
+        internal override string Title => "Avoid Interface with NHibernate Get<> Linq<>";
+        internal override string MessageFormat => "Avoid using an interface as a type argument when retrieving data with NHibernate.";
+        internal override string Description => "NHibernate does not officially support using an interface as the type argument when retrieving data.";
+
+        public override DiagnosticDescriptor Descriptor => new DiagnosticDescriptor(
+            Id,
+            Title,
+            MessageFormat,
+            AnalyzerCategory.Syntax,
+            Severity,
+            true,
+            Description);
+
+        /// <summary>
+        /// Initialize this rule to register it in the context.
+        /// </summary>
+        /// <param name="context"></param>
+        public override void Initialize(AnalysisContext context) {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.TypeArgumentList);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context) {
+            TypeArgumentListSyntax node = context.Node as Microsoft.CodeAnalysis.CSharp.Syntax.TypeArgumentListSyntax;
+            //Get<> and Linq<> can only take one type argument, so we aren't interested in anything else.
+            if (node?.Arguments.Count != 1) {
+                return;
+            }
+
+            ITypeSymbol actualTypeInGeneric = context.SemanticModel.GetTypeInfo(node.Arguments[0]).Type;
+            //If the generic type parameter is not an interface, then this is the correct usage of Linq<> and Get<>
+            if (actualTypeInGeneric.TypeKind != TypeKind.Interface) {
+                return;
+            }
+
+            var memberAccessExpression = node.GetAncestorOfType<MemberAccessExpressionSyntax>();
+
+            //Check that the method name we have is actually Get<> or Linq<>
+            string name = memberAccessExpression?.Name?.ToString();
+            if (string.IsNullOrWhiteSpace(name) || (!name.StartsWith("Get<") && !name.StartsWith("Linq<"))) {
+                return;
+            }
+
+            //Check that the containing type of this method is IQuery, or implements IQuery
+            var symbolInfoOfMethod = context.SemanticModel.GetSymbolInfo(memberAccessExpression);
+            IMethodSymbol memberAccessSymbol = symbolInfoOfMethod.Symbol as IMethodSymbol;
+            if (memberAccessSymbol != null) {
+                ImmutableArray<INamedTypeSymbol> interfaces = memberAccessSymbol.ReceiverType.AllInterfaces;
+                if (!memberAccessSymbol.ReceiverType.ToString().Equals("Aderant.Framework.Persistence.IQuery", StringComparison.Ordinal) && interfaces.All(
+                        typeSymbol => !string.Equals("Aderant.Framework.Persistence.IQuery", typeSymbol.OriginalDefinition.ToString(), StringComparison.Ordinal))) {
+                    return;
+                }
+            } else {
+                //The type did not resolve correctly, but if we only have one candidate, then it is probably that one.
+                ImmutableArray<ISymbol> candidates = symbolInfoOfMethod.CandidateSymbols;
+                if (candidates.Length != 1) {
+                    return;
+                }
+
+                bool thisCandidateIsIquery = false;
+                bool thisCandidateImplementsIquery = false;
+
+                INamedTypeSymbol containingType = candidates.FirstOrDefault()?.ContainingType;
+                if (containingType == null) { //While this should be impossible, I've put it here in case someone else changes the other if statement.
+                    return; //Unable to determine this type if we have no candidate.
+                }
+                //Check to see if the interface we have is Aderant.Framework.Persistence.IQuery
+                thisCandidateIsIquery = containingType.Name.Equals("IQuery", StringComparison.Ordinal) &&
+                                        containingType.ContainingNamespace.ToString().Equals("Aderant.Framework.Persistence", StringComparison.Ordinal);
+                if (!thisCandidateIsIquery) {
+                    //Check to see if the type we have implements Aderant.Framework.Persistence.IQuery
+                    foreach (var iface in containingType.AllInterfaces) {
+                        if (iface.Name.Equals("IQuery") && iface.ContainingNamespace.ToString().Equals("Aderant.Framework.Persistence")) {
+                            thisCandidateImplementsIquery = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!thisCandidateIsIquery && !thisCandidateImplementsIquery) {
+                    return; //It is not of the datatype we care about, so this is not one of the methods we are worried about.
+                }
+            }
+
+            ReportDiagnostic(context, Descriptor, node.Arguments[0].GetLocation(), node.Arguments[0]);
+        }
+
+    }
+}
---
+++ Test/UnitTest.Aderant.Build.Analyzer/Tests/NHibernateRetrieveTypeNotInterfaceRuleTest.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aderant.Build.Analyzer.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+using UnitTest.Aderant.Build.Analyzer.Verifiers;
+
+namespace UnitTest.Aderant.Build.Analyzer.Tests {
+    [TestClass]
+    public class NHibernateRetrieveTypeNotInterfaceRuleTest : AderantCodeFixVerifier {
+        private RuleBase rule;
+        private string postCode;
+
+        protected override RuleBase Rule => new NHibernateRetrieveTypeNotInterfaceRule();
+
+        protected override string PreCode => @"using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+    using System.Data;
+    using System.Data.SqlClient;
+    using Aderant.Framework; 
+    using Aderant.Framework.Persistence;
+    using Aderant.Framework.Persistence.DomainRepsitory;
+    using Aderant.Time.Models;
+
+namespace Aderant.Framework {
+    public struct Identifier {
+        public string StringValue { get; set; }
+        public int IntValue { get; set; }
+    }
+}
+
+namespace Aderant.Framework.Persistence {
+    public interface IQuery {
+        IOrderedQueryable<T> Linq<T>();
+        T Get<T>(Identifier id);
+    }
+
+    public interface IFrameworkSession : IQuery {
+        string Name { get; }
+    }
+
+    public class Session : IFrameworkSession {
+        public IOrderedQueryable<T> Linq<T>() {
+            return null;
+        }
+        public T Get<T>(Identifier id) {
+            return default(T);
+        }
+        public string Name => string.Empty;
+    }
+
+    public interface IRepository : IQuery {
+        bool ExcludeKeys { get; set; }
+    }
+
+    public interface IDecoy {
+        IOrderedQueryable<T> Linq<T>();
+        T Get<T>(Identifier id);
+    }
+
+    public class Decoy : IDecoy {
+        public IOrderedQueryable<T> Linq<T>() {
+            return null;
+        }
+        public T Get<T>(Identifier id) {
+            return default(T);
+        }
+    }
+}
+
+namespace Aderant.Framework.Persistence.DomainRepsitory {
+    public class ObjectRepository : Aderant.Framework.Persistence.IRepository {
+        public IOrderedQueryable<T> Linq<T>() {
+            return null;
+        }
+        public T Get<T>(Identifier id) {
+            return default(T);
+        }
+        public bool ExcludeKeys { get; set; }
+    }
+}
+
+namespace Aderant.Time.Models {
+    public interface ITime {
+        Identifier Id { get; set; }
+        void ResetId();
+    }
+
+    public class Time : ITime {
+        public virtual Identifier Id { get; set; }
+
+        public virtual void ResetId() {
+            Id = new Identifier();
+        }
+    }
+}
+
+namespace ConsoleApplication1 {
+        class PROGRAM {
+            static void Main(string[] args) {
+                ";
+        [TestMethod]
+        public void RepoInterfaceGetIntoInterfaceWithInterface() {
+            const string test = "IRepository provider = new ObjectRepository(); \n" + "ITime timeEntry = provider.Get<ITime>(new Identifier()); ";
+            var expected = GetDiagnostic(MyCodeStartsAtLine + 1, 32);
+            VerifyCSharpDiagnostic(InsertCode(test), expected);
+        }
+        [TestMethod]
+        public void RepoInterfaceGetIntoInterfaceWithConcrete() {
+            const string test = "IRepository provider = new ObjectRepository(); \n" + "ITime timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void RepoInterfaceGetIntoConcreteWithConcrete() {
+            const string test = "IRepository provider = new ObjectRepository(); \n" + "Time timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void RepoConcreteGetIntoInterfaceWithInterface() {
+            const string test = "ObjectRepository provider = new ObjectRepository(); \n" + "ITime timeEntry = provider.Get<ITime>(new Identifier()); ";
+            var expected = GetDiagnostic(MyCodeStartsAtLine + 1, 32);
+            VerifyCSharpDiagnostic(InsertCode(test), expected);
+        }
+        [TestMethod]
+        public void RepoConcreteGetIntoInterfaceWithConcrete() {
+            const string test = "ObjectRepository provider = new ObjectRepository(); \n" + "ITime timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void RepoConcreteGetIntoConcreteWithConcrete() {
+            const string test = "ObjectRepository provider = new ObjectRepository(); \n" + "Time timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+
+        [TestMethod]
+        public void RepoInterfaceLinqIntoInterfaceWithInterface() {
+            const string test = "IRepository provider = new ObjectRepository(); \n" + "ITime timeEntry = provider.Linq<ITime>(new Identifier()); ";
+            DiagnosticResult expected = GetDiagnostic(MyCodeStartsAtLine + 1, 33);
+            VerifyCSharpDiagnostic(InsertCode(test), expected);
+        }
+        [TestMethod]
+        public void RepoInterfaceLinqIntoInterfaceWithConcrete() {
+            const string test = "IRepository provider = new ObjectRepository(); \n" + "ITime timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void RepoInterfaceLinqIntoConcreteWithConcrete() {
+            const string test = "IRepository provider = new ObjectRepository(); \n" + "Time timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void RepoConcreteLinqIntoInterfaceWithInterface() {
+            const string test = "ObjectRepository provider = new ObjectRepository(); \n" + "ITime timeEntry = provider.Linq<ITime>(new Identifier()); ";
+            DiagnosticResult expected = GetDiagnostic(MyCodeStartsAtLine + 1, 33);
+            VerifyCSharpDiagnostic(InsertCode(test), expected);
+        }
+        [TestMethod]
+        public void RepoConcreteLinqIntoInterfaceWithConcrete() {
+            const string test = "ObjectRepository provider = new ObjectRepository(); \n" + "ITime timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void RepoConcreteLinqIntoConcreteWithConcrete() {
+            const string test = "ObjectRepository provider = new ObjectRepository(); \n" + "Time timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+
+        [TestMethod]
+        public void SessionInterfaceGetIntoInterfaceWithInterface() {
+            const string test = "IFrameworkSession provider = new Session(); \n" + "ITime timeEntry = provider.Get<ITime>(new Identifier()); ";
+            DiagnosticResult expected = GetDiagnostic(MyCodeStartsAtLine + 1, 32);
+            VerifyCSharpDiagnostic(InsertCode(test), expected);
+        }
+        [TestMethod]
+        public void SessionInterfaceGetIntoInterfaceWithConcrete() {
+            const string test = "IFrameworkSession provider = new Session(); \n" + "ITime timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void SessionInterfaceGetIntoConcreteWithConcrete() {
+            const string test = "IFrameworkSession provider = new Session(); \n" + "Time timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void SessionConcreteGetIntoInterfaceWithInterface() {
+            const string test = "Session provider = new Session(); \n" + "ITime timeEntry = provider.Get<ITime>(new Identifier()); ";
+            DiagnosticResult expected = GetDiagnostic(MyCodeStartsAtLine + 1, 32);
+            VerifyCSharpDiagnostic(InsertCode(test), expected);
+        }
+        [TestMethod]
+        public void SessionConcreteGetIntoInterfaceWithConcrete() {
+            const string test = "Session provider = new Session(); \n" + "ITime timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void SessionConcreteGetIntoConcreteWithConcrete() {
+            const string test = "Session provider = new Session(); \n" + "Time timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+
+        [TestMethod]
+        public void SessionInterfaceLinqIntoInterfaceWithInterface() {
+            const string test = "IFrameworkSession provider = new Session(); \n" + "ITime timeEntry = provider.Linq<ITime>(new Identifier()); ";
+            DiagnosticResult expected = GetDiagnostic(MyCodeStartsAtLine + 1, 33);
+            VerifyCSharpDiagnostic(InsertCode(test), expected);
+        }
+        [TestMethod]
+        public void SessionInterfaceLinqIntoInterfaceWithConcrete() {
+            const string test = "IFrameworkSession provider = new Session(); \n" + "ITime timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void SessionInterfaceLinqIntoConcreteWithConcrete() {
+            const string test = "IFrameworkSession provider = new Session(); \n" + "Time timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void SessionConcreteLinqIntoInterfaceWithInterface() {
+            const string test = "Session provider = new Session(); \n" + "ITime timeEntry = provider.Linq<ITime>(new Identifier()); ";
+            DiagnosticResult expected = GetDiagnostic(MyCodeStartsAtLine + 1, 33);
+            VerifyCSharpDiagnostic(InsertCode(test), expected);
+        }
+        [TestMethod]
+        public void SessionConcreteLinqIntoInterfaceWithConcrete() {
+            const string test = "Session provider = new Session(); \n" + "ITime timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void SessionConcreteLinqIntoConcreteWithConcrete() {
+            const string test = "Session provider = new Session(); \n" + "Time timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+
+        [TestMethod]
+        public void DecoyInterfaceGetIntoInterfaceWithInterface() {
+            const string test = "IDecoy provider = new Decoy(); \n" + "ITime timeEntry = provider.Get<ITime>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void DecoyInterfaceGetIntoInterfaceWithConcrete() {
+            const string test = "IDecoy provider = new Decoy(); \n" + "ITime timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void DecoyInterfaceGetIntoConcreteWithConcrete() {
+            const string test = "IDecoy provider = new Decoy(); \n" + "Time timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void DecoyConcreteGetIntoInterfaceWithInterface() {
+            const string test = "Decoy provider = new Decoy(); \n" + "ITime timeEntry = provider.Get<ITime>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void DecoyConcreteGetIntoInterfaceWithConcrete() {
+            const string test = "Decoy provider = new Decoy(); \n" + "ITime timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void DecoyConcreteGetIntoConcreteWithConcrete() {
+            const string test = "Decoy provider = new Decoy(); \n" + "Time timeEntry = provider.Get<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+
+        [TestMethod]
+        public void DecoyInterfaceLinqIntoInterfaceWithInterface() {
+            const string test = "IDecoy provider = new Decoy(); \n" + "ITime timeEntry = provider.Linq<ITime>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void DecoyInterfaceLinqIntoInterfaceWithConcrete() {
+            const string test = "IDecoy provider = new Decoy(); \n" + "ITime timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void DecoyInterfaceLinqIntoConcreteWithConcrete() {
+            const string test = "IDecoy provider = new Decoy(); \n" + "Time timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void DecoyConcreteLinqIntoInterfaceWithInterface() {
+            const string test = "Decoy provider = new Decoy(); \n" + "ITime timeEntry = provider.Linq<ITime>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void DecoyConcreteLinqIntoInterfaceWithConcrete() {
+            const string test = "Decoy provider = new Decoy(); \n" + "ITime timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+        [TestMethod]
+        public void DecoyConcreteLinqIntoConcreteWithConcrete() {
+            const string test = "Decoy provider = new Decoy(); \n" + "Time timeEntry = provider.Linq<Time>(new Identifier()); ";
+            VerifyCSharpDiagnostic(InsertCode(test));
+        }
+    }
+}
