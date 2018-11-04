34c44a1ee58381a92d9e130189104e3c99965fb1
Jamie Smillie <jamie.smillie@aderant.com>
2018-11-05 09:01:46 +1300

    Restored EnableConfigDownload switch

--- Src/Build/Functions/Invoke-Build.ps1
+++ Src/Build/Functions/Invoke-Build.ps1
@@ -436,6 +436,9 @@ Should not be used as it prevents incremental builds which increases build times
 
         [Parameter(HelpMessage = "Instructs the console logger to be quiet.")]        
         [switch]$MinimalConsoleLogging,
+
+        [Parameter(HelpMessage = "Enables fetching build configuration files from TFS.")]
+        [switch]$EnableConfigDownload,
         
         [Parameter(ValueFromRemainingArguments)]
         [string[]]$RemainingArgs
