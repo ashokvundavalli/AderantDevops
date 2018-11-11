40a480327743214d1e00e0c0231eb5f0f265aa59
Jamie Smillie <jamie.smillie@aderant.com>
2018-11-12 08:55:27 +1300

    Added Remove-Workspaces script

---
+++ Src/Scripts/Remove-Workspaces.ps1
@@ -0,0 +1,102 @@
+<#
+.SYNOPSIS
+Lists and removes TFS workspace mappings.
+.DESCRIPTION
+Lists and optionally removes TFS workspace mappings for the specified computer name. Requires Visual Studio Team Explorer to function.
+.EXAMPLE
+& '<Path to script>' -computerName $env:COMPUTERNAME -remove
+.EXAMPLE
+& '<Path to script>' -computerName $env:COMPUTERNAME -remove -force
+.PARAMETER computerName
+The computer name to remove associated workspaces from.
+.PARAMETER remove
+Remove workspaces with prompt, unless force is specified.
+.PARAMETER force
+Skip remove confirmation.
+#>
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$computerName,
+    [Parameter(Mandatory=$false)][string[]]$workspaceNames,
+    [switch]$remove,
+    [switch]$force
+)
+
+begin {
+    Set-StrictMode -Version 'Latest'
+    $ErrorActionPreference = 'Stop'
+
+    class Workspace {
+        [string]$Workspace
+        [string]$UserName
+
+        Workspace($workspace, $userName) {
+            $this.Workspace = $workspace
+            $this.UserName = $userName
+        }
+    }
+}
+
+process {
+    try {
+        Get-Command -Name 'TF.exe' -ErrorAction 'SilentlyContinue' | Out-Null
+    } catch {
+        Write-Error "Requires Visual Studio Team Explorer TF.exe to be mapped in the PATH environment variable."
+        exit 1
+    }
+
+    [string[]]$workspaces = $null
+
+    try {
+        $workspaces = tf workspaces /owner:* /computer:$computerName
+    } catch {
+        $Error[0] | Format-List -Force
+        exit 1
+    }
+
+    [System.Collections.Generic.List[Workspace]]$workspaceMappings = @()
+
+    if ($null -ne $workspaces -and $workspaces.Count -gt 3) {
+        for ([int]$i = 3; $i -lt $workspaces.Count; $i++) {
+            [string[]]$values = $workspaces[$i].Split(' ')
+            [void]$workspaceMappings.Add([Workspace]::new($values[0], "$($values[1]).$($values[2])"))
+        }
+    }
+
+    if ($workspaceMappings.Count -eq 0) {
+        Write-Output "No workspace mappings associated with computer name: $computerName"
+        exit 0
+    }
+
+    Write-Output "Workspace mappings associated with computer name: $computerName"
+    Write-Output (Format-Table -InputObject $workspaceMappings)
+
+    if (-not $remove.IsPresent) {
+        exit 0
+    }
+
+    [bool]$error = $false
+
+    for ([int]$i = 0; $i -lt $workspaceMappings.Count; $i++) {
+        if (-not $force.IsPresent) {
+            tf workspace /delete "$($workspaceMappings[$i].Workspace);$($workspaceMappings[$i].UserName)"
+        } else {
+            Write-Output "Removing workspace: $($workspaceMappings[$i].Workspace) assoicated with username: $($workspaceMappings[$i].UserName)"
+
+            try {
+                Write-Output '~' | tf workspace /delete "$($workspaceMappings[$i].Workspace);$($workspaceMappings[$i].UserName)" | Out-Null
+            } catch {
+                $Error[0] | Format-List -Force
+                $error = $true
+            }
+        }
+    }
+}
+
+end {
+    if ($error -eq $false) {
+        exit 0
+    } else {
+        exit 1
+    }
+}
--- Src/Scripts/TFVC-WorkSpace-Initialize.ps1
+++ Src/Scripts/TFVC-WorkSpace-Initialize.ps1
@@ -1,36 +1,35 @@
 [CmdletBinding()]
 param (
-    [Paramter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$workfolder,
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$workfolder,
+    [Parameter(Mandatory=$false)][ValidateNotNullOrEmpty()][string]$workspaceName,
+    [Parameter(Mandatory=$false)][ValidateNotNullOrEmpty()][string]$branch,
     [Parameter(Mandatory=$true)][string[]]$modules
 )
 
-begin {
-    Set-StrictMode -Version 'Latest'
-    $ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 'Latest'
+$ErrorActionPreference = 'Stop'
 
-    [string]$tfsUrl = "http://tfs.$($env:USERDNSDOMAIN.ToLowerInvariant()):8080/tfs/"
-    [string]$workspaceName = [System.Guid]::NewGuid().Guid
+[string]$tfsUrl = "http://tfs.$($env:USERDNSDOMAIN.ToLowerInvariant()):8080/tfs/"
+
+if ($null -eq $workspaceName) {
+    $workspaceName = [System.Guid]::NewGuid().Guid
 }
 
-process {
-    if (-not (Test-Path -Path $workfolder)) {
-        New-Item -Path $workfolder -ItemType Directory -Force
+if (-not (Test-Path -Path $workfolder)) {
+    New-Item -Path $workfolder -ItemType Directory -Force
+}
+
+Push-Location -Path $workfolder
+
+try {
+    TF.exe vc workspace /new "$workspaceName;$env:USERNAME" /collection:$tfsUrl /noprompt
+    TF.exe vc workfold /map "$/ExpertSuite/$($branch.Replace('\', '/'))/Modules" $workfolder /collection:$tfsUrl /workspace:$workspaceName
+
+    foreach ($path in $paths) {
+        TF.exe vc get $paths /recurse /force /noprompt
     }
-
-    Push-Location -Path $workfolder
-
-    try {
-        TF.exe vc workspace /new "$workspaceName;$env:USERNAME" /collection:$tfsUrl /noprompt
-        TF.exe vc workfold /map '$/ExpertSuite/Dev/vnext/Modules' $workfolder /collection:$tfsUrl /workspace:$workspaceName
-
-        foreach ($path in $paths) {
-            TF.exe vc get $paths /recurse /force /noprompt
-        }
-    } finally {
-        TF.exe vc workspace /delete "$workspaceName;$env:USERNAME" /noprompt
-    }
+} finally {
+    TF.exe vc workspace /delete "$workspaceName;$env:USERNAME" /noprompt
 }
 
-end {
-    Pop-Location
-}
+Pop-Location
