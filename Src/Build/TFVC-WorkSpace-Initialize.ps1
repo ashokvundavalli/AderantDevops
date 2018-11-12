5dce99b7b581dc1ded882bb080ece59f7ea05d5b
Jamie Smillie <jamie.smillie@aderant.com>
2018-11-12 14:01:48 +1300

    Added GetTfvc target to ComboBuild

--- Src/Build/TFVC-WorkSpace-Initialize.ps1
+++ Src/Build/TFVC-WorkSpace-Initialize.ps1
@@ -1,25 +1,22 @@
 [CmdletBinding()]
 param (
     [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$workfolder,
-    [Parameter(Mandatory=$false)][ValidateNotNullOrEmpty()][string]$workspaceName,
     [Parameter(Mandatory=$false)][ValidateNotNullOrEmpty()][string]$branch,
-    [Parameter(Mandatory=$true)][string[]]$modules
+    [Parameter(Mandatory=$false)][string]$excludedModules
 )
 
 Set-StrictMode -Version 'Latest'
 $ErrorActionPreference = 'Stop'
 
 [string]$tfsUrl = "http://tfs.$($env:USERDNSDOMAIN.ToLowerInvariant()):8080/tfs/"
-
-if ($null -eq $workspaceName) {
-    $workspaceName = [System.Guid]::NewGuid().Guid
-}
-
+[string]$workspaceName = [System.Guid]::NewGuid().Guid
 [string]$serverPath = "$/ExpertSuite/$($branch.Replace('\', '/'))/Modules"
 
 if (-not (Test-Path -Path $workfolder)) {
     New-Item -Path $workfolder -ItemType Directory -Force
 }
+
+[string[]]$excludedModules = $excludedModules.Split(';')
 
 Push-Location -Path $workfolder
 
@@ -29,8 +26,15 @@ try {
     Write-Output "Using workfolder: $workfolder"
     TF.exe vc workfold /map $serverPath $workfolder /collection:$tfsUrl /workspace:$workspaceName
 
-    foreach ($module in $modules) {
-        TF.exe vc get "$serverPath/$module" /recursive /force /noprompt
+    [string[]]$modules = TF.exe vc dir $serverPath
+
+    for ([int]$i = 1;$i -lt $modules.Length - 2;$i++) {
+        [string]$module = $modules[$i].Replace('$', '')
+        if ($null -eq $excludedModules -or -not $excludedModules.Contains($module)) {
+            TF.exe vc get "$serverPath/$module" /recursive /force /noprompt
+        } else {
+            Write-Output "Excluding module: $module"
+        }
     }
 } finally {
     Write-Output "Removing workspace mapping: $workspaceName;$env:USERNAME"
