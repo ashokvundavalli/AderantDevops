483a4b3877c265c97545bf2d36e69257a5067f00
Jamie Smillie <jamie.smillie@aderant.com>
2018-11-15 09:31:05 +1300

    Added full path to TF.exe

--- Src/Build/TFVC-WorkSpace-Initialize.ps1
+++ Src/Build/TFVC-WorkSpace-Initialize.ps1
@@ -8,6 +8,13 @@ param (
 Set-StrictMode -Version 'Latest'
 $ErrorActionPreference = 'Stop'
 
+[string]$tf = "$($env:VSINSTALLDIR)Common7\IDE\TF.exe"
+
+if (-not (Test-Path $tf)) {
+    Write-Error "Unable to locate TF.exe at path: $tf"
+    exit 1
+}
+
 [string]$tfsUrl = $Env:SYSTEM_TEAMFOUNDATIONSERVERURI
 [string]$workspaceName = [System.Guid]::NewGuid().Guid
 [string]$serverPath = "$/ExpertSuite/$($branch.Replace('\', '/'))/Modules"
@@ -22,23 +29,23 @@ Push-Location -Path $workfolder
 
 try {
     Write-Output "Mapping workspace: $workspaceName;$env:USERNAME"
-    TF.exe vc workspace /new "$workspaceName;$env:USERNAME" /collection:$tfsUrl /noprompt
+    & $tf vc workspace /new "$workspaceName;$env:USERNAME" /collection:$tfsUrl /noprompt
     Write-Output "Using workfolder: $workfolder"
-    TF.exe vc workfold /map $serverPath $workfolder /collection:$tfsUrl /workspace:$workspaceName
+    & $tf vc workfold /map $serverPath $workfolder /collection:$tfsUrl /workspace:$workspaceName
 
-    [string[]]$modules = TF.exe vc dir $serverPath
+    [string[]]$modules = & $tf vc dir $serverPath
 
     for ([int]$i = 1;$i -lt $modules.Length - 2;$i++) {
         [string]$module = $modules[$i].Replace('$', '')
         if ($null -eq $excludedModules -or -not $excludedModules.Contains($module)) {
-            TF.exe vc get "$serverPath/$module" /recursive /force /noprompt
+            & $tf vc get "$serverPath/$module" /recursive /force /noprompt
         } else {
             Write-Output "Excluding module: $module"
         }
     }
 } finally {
     Write-Output "Removing workspace mapping: $workspaceName;$env:USERNAME"
-    TF.exe vc workspace /delete "$workspaceName;$env:USERNAME" /noprompt
+    & $tf vc workspace /delete "$workspaceName;$env:USERNAME" /noprompt
 }
 
 Pop-Location
