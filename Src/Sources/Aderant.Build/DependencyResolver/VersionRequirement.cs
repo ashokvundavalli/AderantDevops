7a6650f0f3de0898617a4ac2ba479c9e69add874
Jamie Smillie <jamie.smillie@aderant.com>
2018-11-19 11:37:58 +1300

    Merged fixes to paket constraint expressions

--- Src/Sources/Aderant.Build/DependencyResolver/VersionRequirement.cs
+++ Src/Sources/Aderant.Build/DependencyResolver/VersionRequirement.cs
@@ -1,9 +1,9 @@
-using System;
-using System.Globalization;
+﻿using System;
+using System.Linq;
 
 namespace Aderant.Build.DependencyResolver {
     internal class ConstraintExpression {
-        private static char[] operators = new char[] {
+        public static char[] Operators { get; } = new char[] {
             '~',
             '=',
             '>',
@@ -11,34 +11,35 @@ namespace Aderant.Build.DependencyResolver {
             '-' // Supports exact match 1.0.0-ci-20170201-223443
         };
 
-        public static char[] Operators {
-            get { return operators; }
-        }
-
         public static string Parse(string versionValue) {
-            if (!string.IsNullOrWhiteSpace(versionValue) && versionValue.IndexOfAny(Aderant.Build.DependencyResolver.ConstraintExpression.Operators) < 0) {
+            if (!string.IsNullOrWhiteSpace(versionValue) && versionValue.IndexOfAny(Operators) < 0) {
                 return "= " + versionValue;
             }
+
             return versionValue;
         }
     }
 
     internal class VersionRequirement : IEquatable<VersionRequirement> {
-    
-
         private string constraintExpression;
 
         public string ConstraintExpression {
             get { return constraintExpression; }
             set {
-                constraintExpression = value;
+                constraintExpression = value.Trim();
 
-                if (!string.IsNullOrWhiteSpace(constraintExpression) && constraintExpression.IndexOfAny(Aderant.Build.DependencyResolver.ConstraintExpression.Operators) < 0) {
-                    if (!string.IsNullOrEmpty(OriginatingFile)) {
-                        throw new InvalidOperationException("The file " + OriginatingFile + " contains an invalid expression \"" + constraintExpression + "\". The expression must contain an operator.");
+                if (!string.IsNullOrWhiteSpace(constraintExpression) && constraintExpression.IndexOfAny(DependencyResolver.ConstraintExpression.Operators) == -1) {
+                    if (constraintExpression.Any(char.IsDigit)) {
+                        // Paket can't deal with '=' characters because reasons.
+                        constraintExpression = DependencyResolver.ConstraintExpression.Parse(constraintExpression);
+                    } else {
+                        if (!string.IsNullOrEmpty(OriginatingFile)) {
+                            throw new InvalidOperationException($"The file: '{OriginatingFile}' contains an invalid expression '{constraintExpression}'. The expression must contain an operator.");
+                        }
+
+                        throw new InvalidOperationException($"Invalid expression '{constraintExpression}'. The expression must contain an operator.");
                     }
-                    throw new InvalidOperationException("Invalid expression \"" + constraintExpression + "\". The expression must contain an operator.");
-                } 
+                }
             }
         }
 
@@ -46,20 +47,30 @@ namespace Aderant.Build.DependencyResolver {
         public string OriginatingFile { get; set; }
 
         public bool Equals(VersionRequirement other) {
-            if (ReferenceEquals(null, other))
+            if (ReferenceEquals(null, other)) {
                 return false;
-            if (ReferenceEquals(this, other))
+            }
+
+            if (ReferenceEquals(this, other)) {
                 return true;
+            }
+
             return string.Equals(ConstraintExpression, other.ConstraintExpression, StringComparison.OrdinalIgnoreCase) && string.Equals(AssemblyVersion, other.AssemblyVersion, StringComparison.OrdinalIgnoreCase);
         }
 
         public override bool Equals(object obj) {
-            if (ReferenceEquals(null, obj))
+            if (ReferenceEquals(null, obj)) {
                 return false;
-            if (ReferenceEquals(this, obj))
+            }
+
+            if (ReferenceEquals(this, obj)) {
                 return true;
-            if (obj.GetType() != this.GetType())
+            }
+
+            if (obj.GetType() != this.GetType()) {
                 return false;
+            }
+
             return Equals((VersionRequirement)obj);
         }
 
@@ -67,14 +78,6 @@ namespace Aderant.Build.DependencyResolver {
             unchecked {
                 return ((ConstraintExpression != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(ConstraintExpression) : 0) * 397) ^ (AssemblyVersion != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(AssemblyVersion) : 0);
             }
-        }
-
-        public static bool operator ==(VersionRequirement left, VersionRequirement right) {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(VersionRequirement left, VersionRequirement right) {
-            return !Equals(left, right);
         }
     }
 }
