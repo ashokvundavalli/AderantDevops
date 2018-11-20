0b36b8db1d6415a5ff931b1a41c42a09ee9870f2
Jamie Smillie <jamie.smillie@aderant.com>
2018-11-20 14:35:18 +1300

    Pimped out TFVC-Convert-Branches script

--- Src/Repository.Tools/TFVC-Convert-Branches.ps1
+++ Src/Repository.Tools/TFVC-Convert-Branches.ps1
@@ -1,26 +1,41 @@
 ﻿param (
-    [Parameter(Mandatory=$true)][string]$moduleName,
+    [Parameter(Mandatory=$false, Position=0)][string]$moduleName,
     [Parameter(Mandatory=$false)][string]$tfsDirectory = "C:\TFS\ExpertSuite",
     [Parameter(Mandatory=$false)][string]$branchName = "Dev/vnext",
     [Parameter(Mandatory=$false)][string]$stagingDirectory = "C:\Temp\Staging",
     [Parameter(Mandatory=$false)][int]$changeSet,
     [switch]$gitIgnore,
-    [switch]$restoreBrances
+    [switch]$restoreBranches
 )
 
 begin {
     Set-StrictMode -Version Latest
 
-    function Restore-Branches {
+    function Restore-Folders {
         param (
-            [Parameter(Mandatory=$true)][string[]]$branches,
-            [Parameter(Mandatory=$true)][string]$moduleName
+            [Parameter(Mandatory=$true)][string[]]$branches
         )
 
         foreach ($branch in $branches) {
             try {
-                Write-Output y|tfpt branches /convertToFolder "$/ExpertSuite/$branch/Modules/$moduleName"
+                Write-Output y|tfpt branches /convertToFolder "$/ExpertSuite/$branch/Modules"
             } catch {
+            }
+        }
+    }
+
+    function Restore-Branches {
+        param (
+            [Parameter(Mandatory=$true)][string[]]$branches,
+            [Parameter(Mandatory=$false)][string]$moduleName
+        )
+
+        if (-not [string]::IsNullOrWhiteSpace($moduleName)) {
+            foreach ($branch in $branches) {
+                try {
+                    Write-Output y|tfpt branches /convertToFolder "$/ExpertSuite/$branch/Modules/$moduleName"
+                } catch {
+                }
             }
         }
     
@@ -34,100 +49,30 @@ begin {
 
     Push-Location -Path $tfsDirectory
 
-    [string[]]$branches = @(
-        "Main",
-        "Dev/801Time",
-        "Dev/802Grad",
-        "Dev/81Expert+",
-        "Dev/83GA",
-        "Dev/Automation",
-        "Dev/BillingBase",
-        "Dev/CaseV1",
-        "Dev/CaseV1ATL",
-        "Dev/ComputedColumns",
-        "Dev/CRTVNext",
-        "Dev/EmployeeIntake",
-        "Dev/Eureka",
-        "Dev/FrameworkNext",
-        "Dev/MP2",
-        "Dev/OTG",
-        "Dev/Packaging",
-        "Dev/PerformanceTest",
-        "Dev/PM",
-        "Dev/QueryService",
-        "Dev/ServiceSimplification",
-        "Dev/Simplification",
-        "Dev/Stability",
-        "Dev/Startup",
-        "Dev/TaskActions",
-        "Dev/TestAutomation",
-        "Dev/Time81",
-        "Dev/TitanSmartForm",
-        "Dev/TitanTime",
-        "Dev/UnicodeComply",
-        "Dev/Upgrade461",
-        "Dev/UXToolkit",
-        "Dev/vnext",
-        "Dev/VS2010",
-        "Dev/Workflow",
-        "Dev/WorkflowData",
-        "Releases/8003",
-        "Releases/8003Patch",
-        "Releases/8004Patch",
-        "Releases/800Hotfix",
-        "Releases/800Patch",
-        "Releases/8011NRF",
-        "Releases/8011Patch",
-        "Releases/801Patch",
-        "Releases/801x",
-        "Releases/8021Atkinsons",
-        "Releases/8021Patch",
-        "Releases/8021SM",
-        "Releases/802Patch",
-        "Releases/802x",
-        "Releases/802xEE",
-        "Releases/8030TaylorWessing",
-        "Releases/8031Patch",
-        "Releases/8032Allens",
-        "Releases/8032Deloitte",
-        "Releases/8032GibsonDunn",
-        "Releases/8032HSF",
-        "Releases/8033Patch",
-        "Releases/8034Patch",
-        "Releases/803x",
-        "Releases/80ServicePack",
-        "Releases/8101Patch",
-        "Releases/8102Patch",
-        "Releases/8110Cleary",
-        "Releases/8110Patch",
-        "Releases/8110Patch2834b",
-        "Releases/811x",
-        "Releases/81x",
-        "Releases/81xHotfix",
-        "Releases/8200Update",
-        "Releases/82x",
-        "Releases/Appstore",
-        "Releases/BurgesSalmon8021",
-        "Releases/CC801Patch",
-        "Releases/GGSP1RTM",
-        "Releases/GGSP2HF01GENU2185",
-        "Releases/GGSP4",
-        "Releases/GGSP4HF01GENU2176",
-        "Releases/GGSP4HF02GENU",
-        "Releases/GGSP4HF03GENU",
-        "Releases/GGSP4HF04GENU",
-        "Releases/GGSP4HF05GENU",
-        "Releases/GGSP4HF1Genu",
-        "Releases/GGSP4Hotfix",
-        "Releases/MatterPlanning"
-    )
+    [System.Collections.ArrayList]$branches = @('Main')
+
+    [string[]]$searchPaths = @('Dev', 'Releases')
+
+    foreach ($searchPath in $searchPaths) {
+        [string[]]$results = TF.exe vc dir "$/ExpertSuite/$searchPath" /deleted
+
+        for ([int]$i = 1; $i -lt $results.Count - 2; $i++) {
+            if ($results[$i].IndexOf(';')) {
+                $branches.Add("$searchPath/$($results[$i].TrimStart().Replace('$', '').Split(';')[0])") | Out-Null
+            } else {
+                $branches.Add($results[$i].TrimStart().Replace('$', '')) | Out-Null
+            }
+        }
+    }
 }
 
 process {
-    if ($restoreBrances.IsPresent) {
+    if ($restoreBranches.IsPresent) {
         Restore-Branches -branches $branches -moduleName $moduleName
         return
     }
+
+    Restore-Folders -branches $branches
 
     foreach ($existingBranch In TFPT.EXE Branches /listBranches:roots) {
         $existingBranch = $existingBranch.TrimStart()
@@ -139,7 +84,7 @@ process {
 
     foreach ($branch in $branches) {
         try {
-            Write-Output y|tfpt branches /convertToBranch "$/ExpertSuite/$branch/Modules/$($moduleName)" /recursive
+            Write-Output y|tfpt branches /convertToBranch "$/ExpertSuite/$branch/Modules/$moduleName" /recursive
         } catch {
         }
     }
