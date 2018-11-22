54af44078603968a350e6e6426583b7056dd7b8a
Jamie Smillie <jamie.smillie@aderant.com>
2018-11-23 09:46:13 +1300

    Merged PR 22128: Stop trolling the TempDBs
    Stop trolling the TempDBs

--- Src/Scripts/cleanup-agent-host.ps1
+++ Src/Scripts/cleanup-agent-host.ps1
@@ -55,7 +55,7 @@ foreach ($dir in $directoriesToRemove) {
 
 # Should a human run this script, don't nuke their environment
 if (-not [System.Environment]::UserInteractive) {
-    Get-PSDrive -PSProvider FileSystem | Select-Object -Property Root | % {$machineWideDirectories += $_.Root + "ExpertShare"}
+    Get-PSDrive -PSProvider FileSystem | Select-Object -Property Root | ForEach-Object {$machineWideDirectories += $_.Root + "ExpertShare"}
 
     # Yay for people who check in PostBuild events :)
     machineWideDirectories += "C:\tfs"
@@ -71,7 +71,9 @@ foreach ($dir in $machineWideDirectories) {
 }
 
 # Clean up databases
-Import-Module SqlServer
-cd sqlserver:\
-cd sql\localhost\default\databases
-gci | % { $_.DropBackupHistory(); $_.Drop() }
+if ($null -eq $env:AgentPool -or $env:AgentPool.Equals('Default')) {
+    Import-Module SqlServer
+    Set-Location sqlserver:\
+    Set-Location sql\localhost\default\databases
+    Get-ChildItem | ForEach-Object { $_.DropBackupHistory(); $_.Drop() }
+}
