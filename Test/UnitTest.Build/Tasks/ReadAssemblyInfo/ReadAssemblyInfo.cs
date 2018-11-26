0e897627c4dc543703e03551419ce5e81069d32b
Michael Baker <michael.baker@aderant.com>
2018-11-26 21:49:10 +1300

    Fix crash during End-to-End integration tests

---
+++ Test/UnitTest.Build/Tasks/ReadAssemblyInfo/ReadAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Build.Tasks.ReadAssemblyInfo {
+
+    [TestClass]
+    public class ReadAssemblyInfoTests {
+
+        [TestMethod]
+        public void Can_parse_file_with_attributes() {
+            var readInfo = new Aderant.Build.Tasks.ReadAssemblyInfo();
+            readInfo.ParseCSharpCode(Resources.AssemblyInfo);
+
+            Assert.IsNotNull(readInfo.AssemblyFileVersion);
+            Assert.IsNotNull(readInfo.AssemblyVersion);
+        }
+    }
+}
