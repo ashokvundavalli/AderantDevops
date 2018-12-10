f9272ade33bc0321d02bd4ac5c634c9752ba06ad
Chuan Wu <chuan.wu@aderant.com>
2018-12-11 11:34:10 +1300

    Adding flag to see if test projects have been impacted by changes

--- Src/Sources/Aderant.Build/ProjectSystem/ConfiguredProject.cs
+++ Src/Sources/Aderant.Build/ProjectSystem/ConfiguredProject.cs
@@ -105,6 +105,11 @@ namespace Aderant.Build.ProjectSystem {
         }
 
         /// <summary>
+        /// Flag for if a test project requires testing.
+        /// </summary>
+        public bool AreTestsImpacted => IsTestProject && BuildReason.Flags != BuildReasonTypes.None;
+
+        /// <summary>
         /// Gets the directory that roots this project.
         /// Driven from the path of <see cref="SolutionFile" />
         /// </summary>
@@ -129,7 +134,7 @@ namespace Aderant.Build.ProjectSystem {
         public IReadOnlyList<string> DirtyFiles {
             get { return dirtyFiles; }
         }
-
+        
         internal BuildReason BuildReason { get; set; }
 
         /// <summary>
