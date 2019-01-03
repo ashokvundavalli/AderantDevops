3b30a2691649e2e29b602be17a7d4979022f910f
Michael Baker <michael.baker@aderant.com>
2019-01-03 16:44:18 +1300

    Drive the modules in build off the make file list

--- Src/Sources/Aderant.Build/ProjectSystem/DirectoryGroveler.cs
+++ Src/Sources/Aderant.Build/ProjectSystem/DirectoryGroveler.cs
@@ -10,16 +10,16 @@ namespace Aderant.Build.ProjectSystem {
     internal class DirectoryGroveler {
         private readonly IFileSystem physicalFileSystem;
 
-        private readonly List<string> extensibilityFiles = new List<string>();
-        private readonly List<string> makeFiles = new List<string>();
-        private readonly List<string> projectFiles = new List<string>();
-        private ICollection<string> directoriesInBuild = new List<string>();
+        private readonly HashSet<string> extensibilityFiles = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        private readonly HashSet<string> makeFiles = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        private readonly HashSet<string> projectFiles = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        private readonly HashSet<string> directoriesInBuild = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
         public DirectoryGroveler(IFileSystem physicalFileSystem) {
             this.physicalFileSystem = physicalFileSystem;
         }
 
-        public ICollection<string> DirectoriesInBuild {
+        public IReadOnlyCollection<string> DirectoriesInBuild {
             get { return directoriesInBuild; }
         }
 
@@ -48,78 +48,22 @@ namespace Aderant.Build.ProjectSystem {
         }
 
         private void AssignPaths(List<string> filePathCollector) {
-            var map = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase) {
-                { "TFSBuild.proj", makeFiles },
-                { "dir.props", extensibilityFiles }
-            };
-
             foreach (var path in filePathCollector) {
-                Log("Found file: " + path);
-
                 string fileName = Path.GetFileName(path);
 
-                List<string> listToAddTo;
-                if (map.TryGetValue(fileName, out listToAddTo)) {
-                    listToAddTo.Add(path);
-                } else {
-                    directoriesInBuild.Add(Path.GetDirectoryName(path));
-
-                    projectFiles.Add(path);
+                if (string.Equals(fileName, "TFSBuild.proj")) {
+                    makeFiles.Add(path);
+                    directoriesInBuild.Add(Path.GetFullPath(Path.GetDirectoryName(path) + "\\..\\"));
+                    continue;
                 }
-            }
 
-            var rootPaths = FindCommonBuildPaths();
-            directoriesInBuild = rootPaths;
-        }
-
-        private HashSet<string> FindCommonBuildPaths() {
-            string commonPath = FindCommonPath(Path.DirectorySeparatorChar.ToString(), directoriesInBuild);
-
-            HashSet<string> rootPaths = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
-
-            var splitChar = new[] { Path.DirectorySeparatorChar };
-
-            foreach (string s in directoriesInBuild) {
-                int pos = s.IndexOf(commonPath, StringComparison.OrdinalIgnoreCase);
-                if (pos == 0) {
-                    string remove = s.Remove(0, commonPath.Length);
-
-                    string[] rootPart = remove.Split(splitChar, StringSplitOptions.RemoveEmptyEntries);
-
-                    rootPaths.Add(Path.Combine(commonPath, rootPart[0]));
-                } else {
-                    rootPaths.Add(s);
+                if (string.Equals(fileName, "dir.props")) {
+                    extensibilityFiles.Add(path);
+                    continue;
                 }
+
+                projectFiles.Add(path);
             }
-
-            return rootPaths;
-        }
-
-        private void Log(string message) {
-            if (Logger != null) {
-                Logger.Info(message);
-            }
-        }
-
-        internal static string FindCommonPath(string separator, ICollection<string> paths) {
-            string commonPath = String.Empty;
-
-            List<string> separatedPath = paths
-                .First(str => str.Length == paths.Max(st2 => st2.Length))
-                .Split(new[] { separator }, StringSplitOptions.RemoveEmptyEntries)
-                .ToList();
-
-            foreach (string pathSegment in separatedPath) {
-                if (commonPath.Length == 0 && paths.All(str => str.StartsWith(pathSegment, StringComparison.OrdinalIgnoreCase))) {
-                    commonPath = pathSegment;
-                } else if (paths.All(str => str.StartsWith(commonPath + separator + pathSegment, StringComparison.OrdinalIgnoreCase))) {
-                    commonPath += separator + pathSegment;
-                } else {
-                    break;
-                }
-            }
-
-            return commonPath;
         }
 
         internal IEnumerable<string> GrovelForFiles(string directory, IReadOnlyCollection<string> excludeFilterPatterns) {
