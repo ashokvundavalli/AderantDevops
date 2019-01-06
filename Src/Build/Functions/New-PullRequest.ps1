0ace537811fd9d353fe8e6b980cbabce932c10b8
Jamie Smillie <jamie.smillie@aderant.com>
2019-01-07 12:00:48 +1300

    Fixed npr, added Migrate-Repositroy function

--- Src/Build/Functions/New-PullRequest.ps1
+++ Src/Build/Functions/New-PullRequest.ps1
@@ -1,16 +1,21 @@
-function New-PullRequest
-{
-    param()
+function New-PullRequest {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false)][ValidateNotNullOrEmpty()]$targetBranch = 'master'
+    )
 
     Set-StrictMode -Version 'Latest'
 
     [string]$currentBranch = git rev-parse --abbrev-ref HEAD
     [string]$repository = git ls-remote --get-url
 
-    if ((git ls-remote --heads $repository $currentBranch) -ne $null) {
-        [string]$url = "http://tfs:8080/tfs/ADERANT/ExpertSuite/_git/$($global:CurrentModuleName)/pullrequestcreate?sourceRef=$($currentBranch)&targetRef=master"
+    if ($null -ne (git ls-remote --heads $repository $currentBranch)) {
+        [string]$url = "http://tfs:8080/tfs/ADERANT/ExpertSuite/_git/$($global:CurrentModuleName)/pullrequestcreate?sourceRef=$($currentBranch)&targetRef=$targetBranch"
         Start-Process $url
     } else {
         Write-Error "No remote branch present. Use git push -u origin $($currentBranch)"
     }
 }
+
+Set-Alias -Name 'npr' -Value 'New-PullRequest'
+Export-ModuleMember -Function 'New-PullRequest' -Alias 'npr'
