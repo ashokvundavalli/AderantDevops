feda46c9308b866871d9d5f4e68e8b48f0039d8e
Michael Baker <michael.baker@aderant.com>
2019-01-09 00:04:08 +1300

    Add a purge script to work around TFS 2018 bugs

---
+++ Src/Scripts/purge-deleted-builds.ps1
@@ -0,0 +1,120 @@
+Set-StrictMode -Version Latest
+$InformationPreference = 'Continue'
+
+$version = "2017-04-17"
+$storageAccount = "expertbuildrete8bb0"
+$storageTable = "builds"
+$table_url = "https://$storageAccount.table.core.windows.net"
+$accesskey = "Te2XHPUov+UvG09q94Kh5Cbl8hHAMlfBQwfGPi9flevbnGBCNaf/c0tJmDwrqJwxCY4HKgGYFfElIlHGCwwTRw=="
+
+function SignContent($resource) {
+    $GMTTime = (Get-Date).ToUniversalTime().ToString('R')
+    $stringToSign = "$GMTTime`n/$storageAccount/$resource"
+    $hmacsha = [System.Security.Cryptography.HMACSHA256]::new()
+    $hmacsha.key = [Convert]::FromBase64String($accesskey)
+    $signature = $hmacsha.ComputeHash([Text.Encoding]::UTF8.GetBytes($stringToSign))
+
+    return [Convert]::ToBase64String($signature),$GMTTime
+}
+
+
+# Based on samples from https://gcits.com/knowledge-base/use-azure-table-storage-via-powershell-rest-api/
+function GetTableEntityAll([string]$tableName) {
+    $resource = $tableName
+    $table_url = "$table_url/$resource"
+    $signature,$GMTTime = SignContent $resource
+    $headers = @{
+        'x-ms-date'    = $GMTTime
+        Authorization  = "SharedKeyLite " + $storageAccount + ":" + $signature
+        "x-ms-version" = $version
+        Accept         = "application/json;odata=fullmetadata"
+    }
+    $item = Invoke-RestMethod -Method Get -Uri $table_url -Headers $headers -ContentType application/json
+    return $item.value
+}
+
+
+function DeleteTableEntity($tableName, $partitionKey, $rowKey) {
+    $resource = "$tableName(PartitionKey='$partitionKey',RowKey='$rowKey')"
+    $table_url = "$table_url/$resource"
+    $signature,$GMTTime = SignContent $resource
+    $headers = @{
+        'x-ms-date'    = $GMTTime
+        Authorization  = "SharedKeyLite " + $storageAccount + ":" + $signature
+        "x-ms-version" = $version
+        Accept         = "application/json;odata=minimalmetadata"
+        'If-Match'     = "*"
+    }
+    $item = Invoke-RestMethod -Method DELETE -Uri $table_url -Headers $headers -ContentType application/http -ErrorAction Stop
+}
+
+
+function DeleteBuildOutputs($pathsToDelete) {
+    $rowKeysToDelete = @()
+    $pathsWithErrors = @()
+
+    foreach ($kvp in $pathsToDelete.GetEnumerator()) {
+        $hadError = $false
+
+        if (Test-Path -LiteralPath $kvp.Key) {
+            try {
+                Remove-Item -LiteralPath $kvp.Key -Force -Recurse -Verbose -WhatIf -ErrorAction Continue
+            } catch {
+                $hadError = $true
+                $pathsWithErrors += $kvp.Key
+            }
+        } else {
+            Write-Information "Path $kvp.Key does not exist"
+        }
+
+        if (-not $hadError) {
+            $rowKeysToDelete += $kvp.Value
+        }
+    }
+
+    return $rowKeysToDelete
+}
+
+# A lookup cache so we don't query the same build more than once
+[Array]$builds = GetTableEntityAll $storageTable
+
+$queryCache = @{}
+$pathsToDelete = @{}
+
+foreach ($tableEntity in $builds) {
+    $buildUrl = $tableEntity.Url
+    $result = $null
+
+    if ($queryCache.ContainsKey($buildUrl)) {
+        $result = $queryCache[$buildUrl]
+    } else {
+        $result = Invoke-RestMethod -Method Get -Uri $buildUrl -UseDefaultCredentials
+        $queryCache.Add($buildUrl, $result)
+    }
+
+    if ($null -ne $result.PSObject.Properties.Item("deleted")) {
+        if ($result.deleted) {
+            if ($result.keepForever) {
+                continue
+            }
+
+            if ($result.retainedByRelease) {
+                continue
+            }
+
+            $artifactPath = $tableEntity.ArtifactPath
+            if (-not $pathsToDelete.ContainsKey($artifactPath)) {
+                $pathsToDelete[$artifactPath] = @()
+            }
+
+            $pathsToDelete[$artifactPath] += $tableEntity.RowKey
+        }
+    }
+}
+
+$rowKeysToDelete = DeleteBuildOutputs $pathsToDelete
+
+foreach ($key in $rowKeysToDelete) {
+    Write-Information "Deleting artifact data with row key: $key"
+    DeleteTableEntity $storageTable "" $key
+}
