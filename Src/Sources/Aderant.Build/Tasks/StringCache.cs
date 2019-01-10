58932a631526b18d72bdee83a23e97b548f2c69c
Michael Baker <michael.baker@aderant.com>
2019-01-10 16:37:58 +1300

    Implement string cache to reduce the cost of SetAssemblySearchPaths target.
    Typical cost is
    347160 ms  SetAssemblySearchPaths                   523 calls

---
+++ Src/Sources/Aderant.Build/Tasks/StringCache.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Microsoft.Build.Framework;
+
+namespace Aderant.Build.Tasks {
+
+    /// <summary>
+    /// Used to cache expensive item groups.
+    /// For example the file list from a directory
+    /// </summary>
+    public class StringCache : Microsoft.Build.Utilities.Task {
+        private static ConcurrentDictionary<string, string> cache = new ConcurrentDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        private bool? hasValueForKey;
+
+        [Required]
+        public string Key {
+            get; set;
+        }
+
+        [Output]
+        public string Value {
+            get {
+                string value;
+                if (cache.TryGetValue(Key, out value)) {
+                    hasValueForKey = true;
+                }
+
+                return value;
+            }
+            set {
+                cache[Key] = value;
+            }
+        }
+
+        [Output]
+        public bool HasValueForKey {
+            get {
+                if (hasValueForKey.HasValue) {
+                    return hasValueForKey.Value;
+                }
+
+                return cache.ContainsKey(Key);
+            }
+        }
+
+        public override bool Execute() {
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
