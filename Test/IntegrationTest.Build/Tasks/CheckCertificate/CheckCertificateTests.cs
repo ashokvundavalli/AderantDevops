9f19721275c30238015b26bb9ec1177474c8f40b
Michael Baker <michael.baker@aderant.com>
2019-01-15 09:52:32 +1300

    Refactor the certificate task. We don't try and reuse the same locals so the code is clearer to read.

--- Test/IntegrationTest.Build/Tasks/CheckCertificate/CheckCertificateTests.cs
+++ Test/IntegrationTest.Build/Tasks/CheckCertificate/CheckCertificateTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IntegrationTest.Build.Tasks.CheckCertificate {
@@ -5,6 +6,9 @@ namespace IntegrationTest.Build.Tasks.CheckCertificate {
     public class CheckCertificateTests : MSBuildIntegrationTestBase {
         [TestMethod]
         public void CheckCertificateTest() {
+            // Dummy reference to mark the code as used.
+            Type type = typeof(Aderant.Build.Tasks.CheckCertificate);
+
             RunTarget("CheckCertificate");
         }
     }
