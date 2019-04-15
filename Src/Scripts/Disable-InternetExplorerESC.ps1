3ad63978af93b69398d363c6e13469ec102e288b
Michael Baker <michael.baker@aderant.com>
2019-04-16 10:05:08 +1200

    Merged PR 24651: Purge the ExpertShare directory
    Purge the ExpertShare directory on agents but only if the drive is local - e.g ignore UNC paths returned from the drive provider.

---
+++ Src/Scripts/Disable-InternetExplorerESC.ps1
@@ -0,0 +1,14 @@
+<#
+    Required for WatiN tests to run correctly.
+#>
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+
+    Stop-Process -Name Explorer
+
+    Write-Information "IE Enhanced Security Configuration (ESC) has been disabled."
+}
+
+Disable-InternetExplorerESC
--- Src/Scripts/cleanup-agent-host.ps1
+++ Src/Scripts/cleanup-agent-host.ps1
@@ -1,83 +1,109 @@
-﻿Start-Transcript -Path ".\CleanupAgentHostLog.txt" -Force
+﻿$root = $PSScriptRoot
+if (-not $root) {
+    $root = $env:TEMP
+}
 
-$directoriesToRemove = @(
-    # Some unit tests aren't very unit-ty
-    "$env:APPDATA\Aderant",
-    "$env:LOCALAPPDATA\Aderant",
+# Should a human run this script, try not to nuke their environment
+$isDesktop = $Env:COMPUTERNAME.StartsWith("WS") -or [System.Environment]::UserInteractive
+
+Set-Location $root
+Microsoft.PowerShell.Host\Start-Transcript -Path ".\CleanupAgentHostLog.txt" -Force
+
+Set-StrictMode -Version Latest
+
+$appDataFolders = @(
+    # Some unit tests aren't very unitty
+    "Aderant",
 
     # NuGet junk drawer
-    "$env:APPDATA\NuGet",
-    "$env:LOCALAPPDATA\NuGet",
-    "$env:USERPROFILE\.nuget",
+    "NuGet",
+    ".nuget",
 
     # Shadow copy cache
-    "$env:LOCALAPPDATA\assembly",
+    "assembly",
 
-    "$env:USERPROFILE\Download",
+    "Temp",
 
-    # VSIX extensions installed by the VS SDK targets
-    "$env:LOCALAPPDATA\Microsoft\VisualStudio\12.0Exp\Extensions\Aderant",
-    "$env:LOCALAPPDATA\Microsoft\VisualStudio\14.0Exp\Extensions\Aderant",
-    "$env:LOCALAPPDATA\Microsoft\VisualStudio\15.0Exp\Extensions\Aderant",
-
-    $env:TEMP,
+    "Microsoft\VisualStudio\12.0Exp\Extensions\Aderant",
+    "Microsoft\VisualStudio\14.0Exp\Extensions\Aderant",
+    "Microsoft\VisualStudio\15.0Exp\Extensions\Aderant",
 
     # Browser and INET stack cache
-    "$env:LOCALAPPDATA\Microsoft\Windows\INetCache"
+    "Microsoft\Windows\INetCache"
+)
+
+$userProfileFolders = @(
+    "Downloads",
+    ".nuget"
 )
 
 $machineWideDirectories = @(
+    $env:TEMP,
     "C:\Temp",
     "C:\Windows\Temp",
     "C:\UIAutomation",
-    "C:\CMS.net",
 
     ([System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory() + "Temporary ASP.NET Files"),
 
     "$Env:WINDIR\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files"
 )
 
-$whoAmI = $env:USERNAME
+
+if ($null -eq $env:AgentPool -or $env:AgentPool -eq 'Default') {
+    $results = Get-PSDrive -PSProvider FileSystem  | Where-Object { $null -eq $_.DisplayRoot -or -not $_.DisplayRoot.StartsWith("\\") } | ForEach-Object { [System.IO.Path]::Combine($_.Root, "ExpertShare") }
+    $machineWideDirectories += $results
+
+    $defaultAgentDirectories = @(
+        "C:\CMS.net",
+
+        # Yay for checked in PostBuild events :)
+        "C:\ExpertShare",
+        "C:\tfs\"
+    )
+
+    $machineWideDirectories += $defaultAgentDirectories
+}
+
+function RemoveFolder([string]$removeTarget) {
+    if (Test-Path $removeTarget) {
+        Write-Information "Deleting files under $removeTarget"
+
+        Remove-Item $removeTarget -Verbose -Force -Recurse -ErrorAction SilentlyContinue
+    } else {
+        Write-Information "Not deleting $removeTarget"
+    }
+}
+
+$currentUserDirectory = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
+$profileHome = [System.IO.Directory]::GetParent($currentUserDirectory)
+
 $serviceAccounts = @("$env:USERNAME", "service.tfsbuild.ap", "tfsbuildservice$", "ExpertService$")
+foreach ($name in $serviceAccounts) {
+    foreach ($folder in $appDataFolders) {
+        RemoveFolder ([System.IO.Path]::Combine($profileHome, $name, "AppData\Local", $folder))
+        RemoveFolder ([System.IO.Path]::Combine($profileHome, $name, "AppData\Roaming", $folder))
+    }
 
-foreach ($dir in $directoriesToRemove) {
-    $removeTarget = $dir
+    foreach ($folder in $userProfileFolders) {
+        RemoveFolder ([System.IO.Path]::Combine($profileHome, $name, $folder))
+    }
+}
 
-    foreach ($name in $serviceAccounts) {
-        $removeTarget = $removeTarget.Replace($whoAmI, $name)
-
-        if (Test-Path $removeTarget) {
-            Write-Output "Deleting files under $removeTarget"
-            Remove-Item $removeTarget -Verbose -Force -Recurse -ErrorAction SilentlyContinue
-        } else {
-            Write-Output "Not deleting $removeTarget"
+if (-not $isDesktop) {
+    foreach ($dir in $machineWideDirectories) {
+        if (Test-Path $dir) {
+            RemoveFolder $dir
         }
     }
-}
 
-# Should a human run this script, don't nuke their environment
-if (-not [System.Environment]::UserInteractive) {
-    if ($null -eq $env:AgentPool -or $env:AgentPool -eq 'Default') {
-        $machineWideDirectories += (Get-PSDrive -PSProvider FileSystem | Select-Object -Property { [System.IO.Path]::Combine($_.Root, "ExpertShare") })
-    }
-
-    # Yay for checked in PostBuild events :)
-    machineWideDirectories += "C:\tfs"
-}
-
-foreach ($dir in $machineWideDirectories) {
-    if (Test-Path $dir) {
-        Push-Location $dir
-        Write-Output "Deleting files under $dir"
-        Remove-Item * -Verbose -Force -Recurse -ErrorAction SilentlyContinue
-        Pop-Location
+    # Clean up databases
+    if ($null -eq $env:AgentPool -or $env:AgentPool.Equals('Default')) {
+        Import-Module SqlServer
+        Set-Location sqlserver:\
+        Set-Location sql\localhost\default\databases
+        Get-ChildItem | ForEach-Object { $_.DropBackupHistory(); $_.Drop() }
     }
 }
 
-# Clean up databases
-if ($null -eq $env:AgentPool -or $env:AgentPool.Equals('Default')) {
-    Import-Module SqlServer
-    Set-Location sqlserver:\
-    Set-Location sql\localhost\default\databases
-    Get-ChildItem | ForEach-Object { $_.DropBackupHistory(); $_.Drop() }
-}
+Set-Location $root
+Microsoft.PowerShell.Host\Stop-Transcript
