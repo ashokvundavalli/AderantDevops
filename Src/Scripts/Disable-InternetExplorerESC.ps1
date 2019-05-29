f3f51d70183ccb396891734b3fd54066c2fdd177
Michael Baker <michael.baker@aderant.com>
2019-05-29 12:30:57 +1200

    Windows 10 support

--- Src/Scripts/Disable-InternetExplorerESC.ps1
+++ Src/Scripts/Disable-InternetExplorerESC.ps1
@@ -4,9 +4,10 @@
 function Disable-InternetExplorerESC {
     $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
 
-    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-
-    Write-Information "IE Enhanced Security Configuration (ESC) has been disabled."
+    if (Test-Path $AdminKey) {
+      Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+      Write-Information "IE Enhanced Security Configuration (ESC) has been disabled."
+    }
 }
 
 Disable-InternetExplorerESC
