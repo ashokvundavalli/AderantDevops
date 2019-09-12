e9d81d005658566a7036fb8b57d5a96e9cf2e06d
Michael Baker <michael.baker@aderant.com>
2019-09-12 13:48:19 +1200

    Upgrade paket for server builds

--- Build/build.ps1
+++ Build/build.ps1
@@ -2,5 +2,5 @@ Set-Location $Env:BUILD_SOURCESDIRECTORY
 
 $buildScriptsDirectory = "$PSScriptRoot\..\src\Build\"
 
-Start-Process -FilePath  "$buildScriptsDirectory\paket.bootstrapper.exe" -ArgumentList  "5.198.0" -NoNewWindow -PassThru -Wait
+Start-Process -FilePath  "$buildScriptsDirectory\paket.bootstrapper.exe" -ArgumentList  "5.219.0" -NoNewWindow -PassThru -Wait
 Start-Process -FilePath  "$buildScriptsDirectory\paket.exe" -ArgumentList "restore" -NoNewWindow -PassThru -Wait
