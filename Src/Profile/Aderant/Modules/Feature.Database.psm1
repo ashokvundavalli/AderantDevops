8ee2022f9d262bc22ff4859d6bc607f9671bd674
Jamie Smillie <jamie.smillie@aderant.com>
2020-05-06 13:45:18 +1200

    Merged PR 32458: Moved Edit-ExpertOwner command to database module
    ### Changes
    * Moved Edit-ExpertOwner command to database module
    * Improved reliability of importing SqlServer module
    
    Related work items: #238940

--- Src/Profile/Aderant/Modules/Feature.Database.psm1
+++ Src/Profile/Aderant/Modules/Feature.Database.psm1
@@ -1,19 +1,19 @@
-<#
-.Synopsis
-    Restores the database for the current local deployment.
-.Description
-    Restores the database for the current local deployment.
-.PARAMETER database
-    The name of the database to restore. Defaults to the current Expert database backup at \\[Computer_Name]\C$\AderantExpert\DatabaseBackups.
-.PARAMETER serverInstance
-    The database server\instance the database is on.
-.PARAMETER backup
-    The database backup to restore. Defaults to \\[Computer_Name]\C$\AderantExpert\DatabaseBackups\[database_name].bak.
-.EXAMPLE
-    Restore-ExpertDatabase -database Expert -databaseServer SVSQL306 -backup C:\Test\DatabaseBackup.bak
-    Will restore the Expert database on to the SVSQL306 SQL server.
-#>
 function global:Restore-ExpertDatabase {
+    <#
+    .Synopsis
+        Restores the database for the current local deployment.
+    .Description
+        Restores the database for the current local deployment.
+    .PARAMETER database
+        The name of the database to restore. Defaults to the current Expert database backup at \\[Computer_Name]\C$\AderantExpert\DatabaseBackups.
+    .PARAMETER serverInstance
+        The database server\instance the database is on.
+    .PARAMETER backup
+        The database backup to restore. Defaults to \\[Computer_Name]\C$\AderantExpert\DatabaseBackups\[database_name].bak.
+    .EXAMPLE
+        Restore-ExpertDatabase -database Expert -databaseServer SVSQL306 -backup C:\Test\DatabaseBackup.bak
+        Will restore the Expert database on to the SVSQL306 SQL server.
+    #>
     [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact='high')]
     param(
         [Parameter(Mandatory=$false)][Alias("name")][string]$database,
@@ -38,8 +38,11 @@ function global:Restore-ExpertDatabase {
             $database = Get-Database
         }
 
-        if (-not (Get-Module -ListAvailable -Name SqlServer)) {
-            Install-Module SqlServer
+        if (-not (Get-Module -ListAvailable -Name 'SqlServer')) {
+            Install-Module -Name 'SqlServer' -Force -AllowClobber
+            Import-Module -Name 'SqlServer'
+        } elseif (-not (Get-Module -Name 'SqlServer')) {
+            Import-Module -Name 'SqlServer'
         }
 
         if ([string]::IsNullOrWhiteSpace($backup)) {
@@ -129,30 +132,30 @@ function global:Restore-ExpertDatabase {
     }
 }
 
-<#
-.Synopsis
-    Backs up the database for the current local deployment.
-.Description
-    Backs up the database for the current local deployment.
-.PARAMETER serverInstance
-    The SQL server\instance the database is on.
-.PARAMETER database
-    The database to backup.
-.PARAMETER backupPath
-    The full file path to backup the database to. Defaults to C:\AderantExpert\DatabaseBackups\[database name].bak.
-.EXAMPLE
-        Backup-ExpertDatabase -databaseServer LocalHost -database Test -backupPath C:\Temp\Test.bak
-    Will backup the ExpertDatabase on SVSQL306 to C:\Temp\Test.bak
-#>
 function global:Backup-ExpertDatabase {
+    <#
+    .Synopsis
+        Backs up the database for the current local deployment.
+    .Description
+        Backs up the database for the current local deployment.
+    .PARAMETER serverInstance
+        The SQL server\instance the database is on.
+    .PARAMETER database
+        The database to backup.
+    .PARAMETER backupPath
+        The full file path to backup the database to. Defaults to C:\AderantExpert\DatabaseBackups\[database name].bak.
+    .EXAMPLE
+            Backup-ExpertDatabase -databaseServer LocalHost -database Test -backupPath C:\Temp\Test.bak
+        Will backup the ExpertDatabase on SVSQL306 to C:\Temp\Test.bak
+    #>
     param(
-        [Parameter(Mandatory=$false)][Alias("name")][String] $database,
+        [Parameter(Mandatory=$false)][Alias("name")][String]$database,
         [Parameter(Mandatory=$false)][string]$serverInstance,
         [Parameter(Mandatory=$false)][string]$backupPath
     )
 
     begin {
-        Set-StrictMode -Version 2.0
+        Set-StrictMode -Version 'Latest'
 
         if ([string]::IsNullOrWhiteSpace($serverInstance)) {
             $serverInstance = Get-DatabaseServer
@@ -197,6 +200,108 @@ function global:Backup-ExpertDatabase {
 
 Export-ModuleMember -Function 'Restore-ExpertDatabase'
 Export-ModuleMember -Function 'Backup-ExpertDatabase'
+
+function global:Edit-ExpertOwner {
+    <#
+    .Synopsis
+        Changes the system owner in FWM_ENVIRONMENT.
+    .Description
+        Changes the system owner in FWM_ENVIRONMENT
+    .PARAMETER owner
+        The owner to set ISSYSTEM = 'Y' for.
+    .PARAMETER ServerInstance
+        The SQL server\instance the database is on.
+    .PARAMETER database
+        The name of the Expert database.
+    .EXAMPLE
+        Edit-ExpertOwner -owner Aderant
+        This will change the system owner to Aderant in the Expert database.
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)][ValidateNotNullOrEmpty()][string]$ServerInstance = $Env:COMPUTERNAME,
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$Database,
+        [switch]$Force
+    )
+
+    dynamicparam {
+        [string]$parameterName = "Owner"
+        $parameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+        $parameterAttribute.Position = 0
+        $parameterAttribute.Mandatory = $true
+        $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+        $attributeCollection.Add($parameterAttribute)
+        $Owners = "Aderant", "Clifford, Maximillian & Scott"
+        $validateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($Owners)
+        $attributeCollection.Add($validateSetAttribute)
+        $runtimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($parameterName, [string], $attributeCollection)
+        $runtimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+        $runtimeParameterDictionary.Add($parameterName, $runtimeParameter)
+        return $runtimeParameterDictionary
+    }
+
+    begin {
+        Set-StrictMode -Version 'Latest'
+        $ErrorActionPreference = 'Stop'
+        $InformationPreference = 'Continue'
+
+        if (-not (Get-Module -ListAvailable -Name 'SqlServer')) {
+            Install-Module -Name 'SqlServer' -Force -AllowClobber
+            Import-Module -Name 'SqlServer'
+        } elseif (-not (Get-Module -Name 'SqlServer')) {
+            Import-Module -Name 'SqlServer'
+        }
+    }
+
+    process {
+        $Owner = $PsBoundParameters[$parameterName]
+
+        Write-Information -MessageData "Server instance set to: $ServerInstance"
+        Write-Information -MessageData "Database set to: $Database"
+        Write-Information -MessageData "Updating Expert owner to: $Owner"
+
+        if ($Owner -contains "Aderant") {
+            [string]$OwnerID = "00000000-0000-0000-0000-00000000000A"
+        } else {
+            [string]$OwnerID = "402A1B6F-AAB2-4B32-BEFD-D4C9BB556029"
+        }
+        
+        [string]$sql = "DECLARE @OWNER NVARCHAR(100) = '" + $Owner + "';
+DECLARE @OWNERID NVARCHAR(40) = '" + $OwnerID + "';
+
+IF NOT EXISTS (SELECT TOP 1 * FROM FWM_OWNER WHERE OWNERID = @OWNERID) BEGIN
+INSERT INTO FWM_OWNER (OWNERID, NAME, ISSYSTEM) VALUES (@OWNERID, @OWNER, 'Y');
+END;
+
+UPDATE FWM_OWNER SET ISSYSTEM = 'Y' WHERE OWNERID = @OWNERID;
+UPDATE FWM_OWNER SET ISSYSTEM = 'N' WHERE OWNERID != @OWNERID;
+UPDATE HBM_PARMS SET FIRM_NAME = @OWNER;"
+    
+        if (-not $Force.IsPresent) {
+            Write-Information -MessageData "Continue?"
+            $answer = Read-Host "Y/N"
+
+            while ("Y", "N" -notcontains $answer) {
+                $answer = Read-Host "Y/N"
+            }
+
+            if ($answer -eq "N") {
+                return
+            }
+        }
+    
+        try {
+            Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $Database -Query $sql
+        } catch {
+            Write-Error "Failed to change Expert owner to: $Owner for database: $Database"
+            return
+        }
+        
+        Write-Information -MessageData "Expert owner set to: $Owner"
+    }
+}
+
+Export-ModuleMember -Function 'Edit-ExpertOwner'
 
 # Provides auto-complete for databases found on SQL Server
 Register-ArgumentCompleter -CommandName "Restore-ExpertDatabase" -ParameterName "database" -ScriptBlock $databaseNameScriptBlock
