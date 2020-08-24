692a76a3255891b6ab51b875321b4c6f4247070a
Michael Baker <michael.baker@aderant.com>
2020-08-24 12:51:01 +1200

    Normalize outputs (Convert / and \) and ensure we ignore trailing slashes.

--- Src/Sources/Aderant.Build/Tasks/GetBuildOutputs.cs
+++ Src/Sources/Aderant.Build/Tasks/GetBuildOutputs.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Aderant.Build.ProjectSystem;
+using Aderant.Build.Utilities;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 namespace Aderant.Build.Tasks {
     public class GetBuildOutputs : BuildOperationContextTask {
         private List<OnDiskProjectInfo> projects;
+
+        public string[] SolutionRoot { get; set; }
+
+        public bool NormalizeOutputPath { get; set; }
 
         [Output]
         public string[] SolutionRoots { get; private set; }
@@ -22,9 +27,20 @@ namespace Aderant.Build.Tasks {
         public override bool ExecuteTask() {
             projects = PipelineService.GetTrackedProjects().ToList();
 
+            if (SolutionRoot != null) {
+                projects = projects.Where(s => {
+                    foreach (var item in SolutionRoot) {
+                        if (PathUtility.PathComparer.Equals(item, s.SolutionRoot)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }).ToList();
+            }
+
             SolutionRoots = projects
                 .Select(s => s.SolutionRoot)
-                .Distinct(StringComparer.OrdinalIgnoreCase)
+                .Distinct(PathUtility.PathComparer)
                 .ToArray();
 
             return true;
@@ -32,6 +48,7 @@ namespace Aderant.Build.Tasks {
 
         private ITaskItem[] CreateProjectTaskItems() {
             List<ITaskItem> projectTaskItems = new List<ITaskItem>();
+            HashSet<string> s = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
             foreach (var project in projects) {
                 var taskItem = new TaskItem(project.FullPath);
@@ -39,7 +56,16 @@ namespace Aderant.Build.Tasks {
                 string directoryOfFile = Path.GetDirectoryName(project.FullPath);
                 string outputPath = Path.Combine(directoryOfFile, project.OutputPath);
 
-                taskItem.SetMetadata("OutputPath", Path.GetFullPath(outputPath));
+                if (NormalizeOutputPath) {
+                    outputPath = PathUtility.EnsureTrailingSlash(Path.GetFullPath(outputPath));
+                }
+
+                if (!s.TryGetValue(outputPath, out var actualOutputPath)) {
+                    s.Add(outputPath);
+                    actualOutputPath = outputPath;
+                }
+
+                taskItem.SetMetadata("OutputPath", actualOutputPath);
                 taskItem.SetMetadata("SolutionRoot", project.SolutionRoot);
 
                 projectTaskItems.Add(taskItem);
