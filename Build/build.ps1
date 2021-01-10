8788d82de65127468db7dcf3890c838edcfe5049
Jamie Smillie <jamie.smillie@aderant.com>
2021-01-11 11:33:56 +1300

    Merged PR 36022: Set PAKET_SKIP_RESTORE_TARGETS environment variable
    Set PAKET_SKIP_RESTORE_TARGETS environment variable
    
    Related work items: #248409

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,13 +1,17 @@
 # This script is used to bootstrap the compile process for Build.Infrastructure on the server.
-Set-Location -Path ($PSScriptRoot + "\..\")
+Set-Location -Path ([System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "..\")))
 [string]$buildScriptsDirectory = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\Src\Build\")
+[string]$paketBootstrapper = [System.IO.Path]::Combine($buildScriptsDirectory, 'paket.bootstrapper.exe')
+
+[System.Environment]::SetEnvironmentVariable('PAKET_SKIP_RESTORE_TARGETS', 'true', [System.EnvironmentVariableTarget]::Process)
+
+# Update Paket bootstrapper.
+[void](Start-Process -FilePath $paketBootstrapper -ArgumentList @('--self') -NoNewWindow -PassThru -Wait)
 
 # Load the version of Paket we want to use. Released versions can be found here: https://github.com/fsprojects/Paket/releases
 [string]$paketVersion = Get-Content -Path "$PSScriptRoot\paket.version"
-
-# Update Paket bootstrapper.
-Start-Process -FilePath  "$buildScriptsDirectory\paket.bootstrapper.exe" -ArgumentList '--self' -NoNewWindow -PassThru -Wait | Out-Null
 # Download the version of Paket we're using.
-Start-Process -FilePath  "$buildScriptsDirectory\paket.bootstrapper.exe" -ArgumentList $paketVersion -NoNewWindow -PassThru -Wait | Out-Null
+[void](Start-Process -FilePath  $paketBootstrapper -ArgumentList @($paketVersion) -NoNewWindow -PassThru -Wait)
+
 # Run Paket restore.
-Start-Process -FilePath  "$buildScriptsDirectory\paket.exe" -ArgumentList 'restore' -NoNewWindow -PassThru -Wait | Out-Null
+[void](Start-Process -FilePath  "$buildScriptsDirectory\paket.exe" -ArgumentList @('restore') -NoNewWindow -PassThru -Wait)
