102109c6ab174d8d906e8f618a2f578d1e6fbee3
Ji Paik <ji.paik@aderant.com>
2021-03-11 11:43:35 +1300

    Merged PR 36829: Add script for drop cleanup
    Add script for drop cleanup
    
    Related work items: #251149

---
+++ Src/Scripts/drop-cleanup.ps1
@@ -0,0 +1,14 @@
+﻿Set-StrictMode -Version Latest
+$VerbosePreference = 'Continue'
+
+Function DFS {
+    Param([string]$Path)
+    $directories = @(Get-ChildItem -Path $Path -Directory -Force -ErrorAction SilentlyContinue)
+    $dirs = $directories | Where-Object { @(Get-ChildItem $_.fullName).count -eq 0 } | Select-Object -expandproperty FullName
+    $dirs | Foreach-Object { Remove-Item $_ -Verbose }
+    foreach($d in $directories) { DFS $d.fullname }
+}
+
+DFS('\\dfs.aderant.com\expert-ci\pulls')
+DFS('\\dfs.aderant.com\expert-ci\product')
+DFS('\\dfs.aderant.com\expert-ci\prebuilts\v1\')
