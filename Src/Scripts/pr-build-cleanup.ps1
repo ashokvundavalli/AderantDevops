a6fc52f6c949f59506baa73875567d26ec695363
Ji Paik <ji.paik@aderant.com>
2021-03-11 10:29:18 +1300

    Merged PR 36740: Add script to cleanup completed PR Builds
    Add script to cleanup completed PR Builds
    
    Related work items: #250610

---
+++ Src/Scripts/pr-build-cleanup.ps1
@@ -0,0 +1,38 @@
+$stem = "http://tfs:8080/tfs/ADERANT/ExpertSuite/_apis/"
+$done = [System.Collections.Generic.HashSet[int]]::new()
+
+function DeleteBuild($data) {
+    if ($data.count -eq 0) {
+        return
+    }
+    foreach ($pr in $data.value) {
+        $prId = $pr.pullRequestId
+         
+        if (($done.Add($prId))) {             
+            $branchName = "refs/pull/$prId/merge"
+            $branchName = [System.Web.HTTPUtility]::UrlEncode($branchName)
+            $buildsForPullRequest = Invoke-RestMethod -Method GET "$stem/build/builds?repositoryId=ExpertSuite&repositoryType=TfsGit&branchName=$branchName&api-version=2.0" -UseDefaultCredentials -ErrorAction Continue
+            $builds = $buildsForPullRequest.value
+            foreach ($build in $builds) {        
+                if (-not $build.keepForever -and -not $build.retainedByRelease) {
+                    Invoke-RestMethod -Method Delete "$($build.url)?api-version=2.0" -UseDefaultCredentials -ErrorAction Continue    
+                }
+            }
+        }
+    }
+}
+
+$skip = 0
+$top = 100
+$branchName = [System.Web.HTTPUtility]::UrlEncode("refs/heads/master")
+while ($true) {
+    $data = Invoke-RestMethod -Method GET "$stem/git/repositories/ExpertSuite/pullRequests?searchCriteria.status=completed&searchCriteria.targetRefName=$branchName&top=$top&skip=$skip&api-version=2.0" -UseDefaultCredentials
+    if ($data.count -eq 0) {
+        break
+    }
+    DeleteBuild $data
+    $skip = $skip + $top
+    if ($skip -ge 50000) {
+        break
+    }
+}
