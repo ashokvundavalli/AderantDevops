ecdd580fdd6cbcfce29de622eac6c6c8ca4b77a5
Jamie Smillie <jamie.smillie@aderant.com>
2021-04-14 10:20:30 +1200

    Merged PR 37266: Remove redundant code relating to git prompt
    Remove redundant code relating to git prompt.
    
    The window title can be configured in the [Install-PoshGit.ps1](https://tfs.aderant.com/tfs/ADERANT/ExpertSuite/DevOps/_git/Build.Infrastructure?path=%2FSrc%2FProfile%2FAderant%2FFunctions%2FInstall-PoshGit.ps1&version=GBmaster) script by changing the ```$global:GitPromptSettings.EnableWindowTitle``` variable.
    
    It is currently set to:
    ```PowerShell
    "PowerShell $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor) - "
    ```
    
    ### With changes
    ![image.png](https://tfs.aderant.com/tfs/ADERANT/44f228f7-b636-4bd3-99ee-eb2f1570d768/_apis/git/repositories/a0f7d5b4-270c-450d-9f90-d8b8e31d4a2e/pullRequests/37266/attachments/image.png)
    ### master
    ![image.png](https://tfs.aderant.com/tfs/ADERANT/44f228f7-b636-4bd3-99ee-eb2f1570d768/_apis/git/repositories/a0f7d5b4-270c-450d-9f90-d8b8e31d4a2e/pullRequests/37266/attachments/image%20(3).png)
    
    ### Changes
    - Require Git module to be run as Administrator
    - The window title is handled by Posh-Git, so removed duplicated effort
    
    Related work items: #251312

--- Src/Profile/Aderant/Git.psm1
+++ Src/Profile/Aderant/Git.psm1
@@ -1,37 +1,27 @@
-$currentUser = [Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())
-$isAdminProcess = $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-$adminHeader = if ($isAdminProcess) { 'Administrator: ' } else { [string]::Empty }
+#Requires -RunAsAdministrator
 
 function global:Enable-GitPrompt {
     Function global:Prompt {
-        $realLASTEXITCODE = $LASTEXITCODE
+        [int]$existingExitCode = $global:LASTEXITCODE
 
-        [string]$location = Get-Location
-        
         Write-Host ([string]::Empty)
         Write-Host 'Module [' -NoNewline
-        Write-Host ($global:ShellContext.CurrentModuleName) -NoNewline -ForegroundColor DarkCyan
+        Write-Host ($global:ShellContext.CurrentModuleName) -NoNewline -ForegroundColor 'DarkCyan'
         Write-Host '] at [' -NoNewline
-        Write-Host ($global:ShellContext.CurrentModulePath) -NoNewline -ForegroundColor DarkCyan
+        Write-Host ($global:ShellContext.CurrentModulePath) -NoNewline -ForegroundColor 'DarkCyan'
         Write-Host ']'
-        Write-Host "PS ${location}" -NoNewline
+        Write-Host "PS $((Get-Location).Path)" -NoNewline
 
         if ($global:ShellContext.PoshGitAvailable) {
             Write-VcsStatus
-
-            $status = Get-GitStatus
-
-            if ($null -ne $status) {
-                $repoName = Split-Path -Leaf (Split-Path $status.GitDir)
-                $Host.UI.RawUI.WindowTitle = "$script:adminHeader$repoName [$($status.Branch)]"
-            }
         }
 
         Write-Host  "$('>' * ($nestedPromptLevel + 1))" -NoNewline
 
-        $global:LASTEXITCODE = $realLASTEXITCODE
+        # Restore LASTEXITCODE to its original value.
+        $global:LASTEXITCODE = $existingExitCode
 
-        # Default console looks like this
+        # Default console looks like this:
         # PS C:\WINDOWS\system32>
         return ' '
     }
