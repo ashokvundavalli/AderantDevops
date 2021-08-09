d437426440f3872c8ebd74e95cb2ff3ea39880c8
Jamie Smillie <jamie.smillie@aderant.com>
2021-08-10 07:57:27 +1200

    Reverted changes to iis-cleanup script

--- Src/Scripts/iis-cleanup.ps1
+++ Src/Scripts/iis-cleanup.ps1
@@ -1,9 +1,9 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = Continue
-$InformationPreference = Continue
+Set-StrictMode -Version 'Latest'
+$ErrorActionPreference = 'Continue'
+$InformationPreference = 'Continue'
 
 if (-not (Get-Module -Name 'IISAdministration' -ListAvailable)) {
-    Install-Module -Name 'IISAdministration' -Scope AllUsers -AllowClobber -Force
+    Install-Module -Name 'IISAdministration' -AllowClobber -Force
 }
 
 if (-not (Get-Module -Name 'IISAdministration')) {
@@ -22,29 +22,29 @@ if (-not (Get-Module -Name 'ApplicationServer')) {
 $expertWebApplications = Get-ASApplication -SiteName 'Default Web Site'
 
 foreach ($webApp in $expertWebApplications) {
-    if (-not ((Test-Path $webApp.IISPath) -band (Test-Path $($webApp.PhysicalPath)))) {
-        if ($webApp.ApplicationName) {
-            $iisPath = $webApp.IISPath
-            Remove-Item -Path $iisPath -Force
-            Write-Information -MessageData "Removed web application: '$iisPath'."
-            # The WebConfigurationLocation may not exist for some paths.
-            Remove-WebConfigurationLocation -Name $webApp.IISPath -WarningAction SilentlyContinue
-        }
-    }
+	if (-not ((Test-Path $webApp.IISPath) -band (Test-Path $($webApp.PhysicalPath)))) {
+		if ($webApp.ApplicationName) {
+			$iisPath = $webApp.IISPath
+			Remove-Item -Path $iisPath -Force
+			Write-Information -MessageData "Removed web application: '$iisPath'."
+			# The WebConfigurationLocation may not exist for some paths.
+			Remove-WebConfigurationLocation -Name $webApp.IISPath -WarningAction 'SilentlyContinue'
+		}
+	}
 }
 
 if ($null -ne $Env:AgentPool -and $Env:AgentPool -eq 'Test') {
-    try {
-        # Attempt to start app pools.
-        Start-WebAppPool -Name 'Expert*'
-    } catch {
-        # Ignore any errors - app pools may not exist.
-    }
+	try {
+		# Attempt to start app pools.
+		Start-WebAppPool -Name Expert*
+	} catch {
+		# Ignore any errors - app pools may not exist.
+	}
 } else {
-    $applicationPools = Get-IISAppPool | Where-Object { $_.Name -match 'Expert' }
+	$applicationPools = Get-IISAppPool | Where-Object { $_.Name -match 'Expert' }
 
-    foreach ($applicationPool in $applicationPools) {
-        Write-Information -MessageData "Removing application pool: '$($applicationPool.Name)'."
-        $applicationPool.Delete()
-    }
+	foreach ($applicationPool in $applicationPools) {
+		Write-Information -MessageData "Removing application pool: '$($applicationPool.Name)'."
+		$applicationPool.Delete()
+	}
 }
