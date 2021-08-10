289e0a6d613a4bd3dfcd2e4c84b7d8eadbb4116d
Jamie Smillie <jamie.smillie@aderant.com>
2021-08-11 10:50:02 +1200

    Merged PR 38756: Check if IIS is installed when running cleanup script
    Check if IIS is installed when running cleanup script
    
    Related work items: #252041

--- Src/Scripts/iis-cleanup.ps1
+++ Src/Scripts/iis-cleanup.ps1
@@ -1,6 +1,21 @@
 Set-StrictMode -Version 'Latest'
 $ErrorActionPreference = 'Continue'
 $InformationPreference = 'Continue'
+$ProgressPreference = 'SilentlyContinue'
+
+if ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name 'ProductName').ProductName -match 'Windows Server') {
+    # Windows Server OS
+    if (-not (Get-WindowsFeature -Name 'Web-Server').Installed) {
+        Write-Host 'IIS is not installed.'
+        return
+    }
+} else {
+    # Windows Client OS
+    if ((Get-WindowsOptionalFeature -Online -FeatureName 'IIS-WebServerRole').State -ne 'Enabled') {
+        Write-Host 'IIS is not installed.'
+        return
+    }
+}
 
 if (-not (Get-Module -Name 'IISAdministration' -ListAvailable)) {
     Install-Module -Name 'IISAdministration' -AllowClobber -Force
