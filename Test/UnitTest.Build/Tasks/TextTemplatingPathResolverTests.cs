7824346d92db0623792783da29c6b595816b8996
Michael Baker <michael.baker@aderant.com>
2021-09-30 11:21:08 +1300

    Fix up reflection constructor call

---
+++ Test/UnitTest.Build/Tasks/TextTemplatingPathResolverTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aderant.Build.Tasks.TextTemplating;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Build.Tasks {
+    [TestClass]
+    public class TextTemplatingPathResolverTests {
+
+        [TestMethod]
+        public void Can_construct_visual_studio_instance() {
+            var visualStudioInstance = TextTemplatingPathResolver.Create2015Install("C:\\SomeDirectory");
+        }
+
+    }
+}
