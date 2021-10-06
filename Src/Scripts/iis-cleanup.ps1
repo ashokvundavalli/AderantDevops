ff72541f7a7963ab65f3f04e197826da1ac5bbc7
Michael Baker <michael.baker@aderant.com>
2021-10-06 18:40:10 +1300

    Temp

--- Src/Scripts/iis-cleanup.ps1
+++ Src/Scripts/iis-cleanup.ps1
@@ -31,32 +31,32 @@ try {
    Write-Verbose -Message "Stopping IIS while removing build agent directories to prevent file locks."
    iisreset.exe /STOP
 
-    foreach ($webApp in $site.Applications | Where-Object { $_.Path -like "/Expert_*" }) {
-        # The WebConfigurationLocation may not exist for some paths.
-        # The value "Name" should match exactly with the XML element in applicationHost.config (so trim the trailing slash)
-        Remove-WebConfigurationLocation -Name $webApp.VirtualDirectories[0].ToString().TrimEnd("/")
-
-        Remove-WebApplication -Name $webApp.Path -Site $iisSite -Verbose
-
-        foreach ($virtualDirectory in $webApp.VirtualDirectories) {
-            Remove-Item -Path $virtualDirectory.PhysicalPath -Force -Verbose
-        }
-    }
-
-    if ($null -ne $Env:AgentPool -and $Env:AgentPool -eq "Test") {
-        try {
-            # Attempt to start app pools.
-            Start-WebAppPool -Name Expert*
-        } catch {
-            # Ignore any errors - app pools may not exist.
-        }
-    } else {
-        $applicationPools = Get-IISAppPool | Where-Object { $_.Name -like "Expert*" }
-
-        foreach ($applicationPool in $applicationPools) {
-            Remove-WebAppPool $applicationPool.Name
-        }
-    }
+    #foreach ($webApp in $site.Applications | Where-Object { $_.Path -like "/Expert_*" }) {
+    #    # The WebConfigurationLocation may not exist for some paths.
+    #    # The value "Name" should match exactly with the XML element in applicationHost.config (so trim the trailing slash)
+    #    Remove-WebConfigurationLocation -Name $webApp.VirtualDirectories[0].ToString().TrimEnd("/")
+    #
+    #    Remove-WebApplication -Name $webApp.Path -Site $iisSite -Verbose
+    #
+    #    foreach ($virtualDirectory in $webApp.VirtualDirectories) {
+    #        Remove-Item -Path $virtualDirectory.PhysicalPath -Force -Verbose
+    #    }
+    #}
+    #
+    #if ($null -ne $Env:AgentPool -and $Env:AgentPool -eq "Test") {
+    #    try {
+    #        # Attempt to start app pools.
+    #        Start-WebAppPool -Name Expert*
+    #    } catch {
+    #        # Ignore any errors - app pools may not exist.
+    #    }
+    #} else {
+    #    $applicationPools = Get-IISAppPool | Where-Object { $_.Name -like "Expert*" }
+    #
+    #    foreach ($applicationPool in $applicationPools) {
+    #        Remove-WebAppPool $applicationPool.Name
+    #    }
+    #}
 
 
     # Result of bug 257338
