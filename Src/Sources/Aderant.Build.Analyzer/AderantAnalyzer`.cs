9a9fc4f63420e298f701a8af7071c396588a7ffc
Michael Baker <michael.baker@aderant.com>
2022-05-13 11:01:26 +1200

    QFE: The test analyzer should not be exported.

--- Src/Sources/Aderant.Build.Analyzer/AderantAnalyzer`.cs
+++ Src/Sources/Aderant.Build.Analyzer/AderantAnalyzer`.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Aderant.Build.Analyzer.Rules;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Diagnostics;
+﻿using Aderant.Build.Analyzer.Rules;
 
 namespace Aderant.Build.Analyzer {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class AderantAnalyzer<T> : AderantAnalyzer where T : RuleBase, new() {
 
         public AderantAnalyzer() : base(new T()) {
         }
-
     }
 }
--- Src/Sources/Aderant.Build.Analyzer/Properties/AssemblyInfo.cs
+++ Src/Sources/Aderant.Build.Analyzer/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Aderant.Analyzer")]
 
 [assembly: AssemblyCompany("Aderant Holdings, Inc.")]
-[assembly: AssemblyProduct("Aderant.Analyzer")]
-[assembly: AssemblyCopyright("Copyright © ADERANT 2061-2022")]
+[assembly: AssemblyProduct("Aderant.Build.Analyzer")]
+[assembly: AssemblyCopyright("Copyright © ADERANT 2016-2022")]
 [assembly: AssemblyTrademark("Aderant Expert is a trademark of Aderant Holdings, Inc.")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("3.3.1.0")]
-[assembly: AssemblyFileVersion("3.3.1.0")]
+[assembly: AssemblyVersion("3.4.1.0")]
+[assembly: AssemblyFileVersion("3.4.1.0")]
 [assembly: InternalsVisibleTo("UnitTest.Aderant.Build.Analyzer")]
 [assembly: InternalsVisibleTo("IntegrationTest.Build")]
 [assembly: InternalsVisibleTo("IntegrationTest.Analyzer")]
